b18080ca7e1303ae40c364c206d3d0aae8259d46
Tristan Harward <Tristan Harward@46129fc4-274e-7f48-8cfa-5da4c607681c>
2005-08-12 19:32:35 +0000

    Themes
    git-svn-id: http://www.zenphoto.org/svn@45 46129fc4-274e-7f48-8cfa-5da4c607681c

--- index.php
+++ index.php
@@ -1,41 +1,17 @@
-<?php require_once("zen/template-functions.php"); /* Don't put anything before this line! */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-<head>
-  <title><?php printGalleryTitle(); ?></title>
-  <link rel="stylesheet" href="/zp/zen.css" type="text/css" />
-  <?php zenJavascript(); ?>
-</head>
-<body>
-<div id="main">
-  <div id="gallerytitle">
-    <h2><?php echo getGalleryTitle(); ?></h2>
-  </div>
-  
-  <hr />
-  
-  <?php printPageListWithNav("&laquo; prev", "next &raquo;"); ?>
-  
-  
-  <div id="albums">
-  <?php while (next_album()): ?>
-  
-    <div class="album">
-      <div class="albumthumb"><a href="<?=getAlbumLinkURL();?>" title="<?=getAlbumTitle();?>">
-        <?php printAlbumThumbImage(getAlbumTitle()); ?></a></div>
-      <div class="albumtitle"><h3><a href="<?=getAlbumLinkURL();?>" title="<?=getAlbumTitle();?>">
-        <?php printAlbumTitle(); ?></a></h3> <?php printAlbumDate(); ?></div>
-      <div class="albumdesc"><?php printAlbumDesc(); ?></div>
-    </div>
-    <hr />
-    
-  <?php endwhile; ?>
-  </div>
-  
-  <?php printPageNav("&laquo; prev", "|", "next &raquo;"); ?>
+<?php 
+require_once("zen/template-functions.php");
+$themepath = 'themes';
+$theme = 'default';
+
+$_zp_themeroot = WEBPATH . "/$themepath/$theme";
+
+if (in_context(ZP_IMAGE)) {
+  include("$themepath/$theme/image.php");
+} else if (in_context(ZP_ALBUM)) {
+  include("$themepath/$theme/album.php");
+} else if (in_context(ZP_INDEX)) {
+  include("$themepath/$theme/index.php");
+}
 
 
-</div>
-
-</body>
-</html>
+?>
