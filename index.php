86402247a575f27d28b235b2d45fb446d4195d77
luckyspin <luckyspin@46129fc4-274e-7f48-8cfa-5da4c607681c>
2006-04-13 07:06:29 +0000

    Moved remotely
    git-svn-id: http://www.zenphoto.org/svn/trunk@178 46129fc4-274e-7f48-8cfa-5da4c607681c

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+require_once("zen/template-functions.php");
+$themepath = 'themes';
+
+$theme = $_zp_gallery->getCurrentTheme();
+
+$_zp_themeroot = WEBPATH . "/$themepath/$theme";
+
+if (in_context(ZP_IMAGE)) {
+  include("$themepath/$theme/image.php");
+} else if (in_context(ZP_ALBUM)) {
+  include("$themepath/$theme/album.php");
+} else if (in_context(ZP_INDEX)) {
+  include("$themepath/$theme/index.php");
+}
+
+
+?>
