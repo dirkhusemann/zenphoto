9e02a24c30ab77c8ed7da57a698c8ad94fcdcd66
trisweb <trisweb@46129fc4-274e-7f48-8cfa-5da4c607681c>
2006-04-29 22:47:57 +0000

    New version 0.12 of the Sajax library.
    git-svn-id: http://www.zenphoto.org/svn/branches/subalbums@201 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zen/Sajax.php
+++ zen/Sajax.php
@@ -5,25 +5,75 @@ if (!isset($SAJAX_INCLUDED)) {
 	 * GLOBALS AND DEFAULTS
 	 *
 	 */ 
-	$sajax_debug_mode = 0;
-	$sajax_export_list = array();
-	$sajax_request_type = "GET";
-	$sajax_remote_uri = "";
+	$GLOBALS['sajax_version'] = '0.12';	
+	$GLOBALS['sajax_debug_mode'] = 0;
+	$GLOBALS['sajax_export_list'] = array();
+	$GLOBALS['sajax_request_type'] = 'GET';
+	$GLOBALS['sajax_remote_uri'] = '';
+	$GLOBALS['sajax_failure_redirect'] = '';
 	
 	/*
 	 * CODE
 	 *
 	 */ 
+	 
+	//
+	// Initialize the Sajax library.
+	//
 	function sajax_init() {
 	}
 	
+	//
+	// Helper function to return the script's own URI. 
+	// 
 	function sajax_get_my_uri() {
-		global $REQUEST_URI;
-		
-		return $REQUEST_URI;
+		return $_SERVER["REQUEST_URI"];
 	}
-	
 	$sajax_remote_uri = sajax_get_my_uri();
+	
+	//
+	// Helper function to return an eval()-usable representation
+	// of an object in JavaScript.
+	// 
+	function sajax_get_js_repr($value) {
+		$type = gettype($value);
+		
+		if ($type == "boolean") {
+			return ($value) ? "Boolean(true)" : "Boolean(false)";
+		} 
+		elseif ($type == "integer") {
+			return "parseInt($value)";
+		} 
+		elseif ($type == "double") {
+			return "parseFloat($value)";
+		} 
+		elseif ($type == "array" || $type == "object" ) {
+			//
+			// XXX Arrays with non-numeric indices are not
+			// permitted according to ECMAScript, yet everyone
+			// uses them.. We'll use an object.
+			// 
+			$s = "{ ";
+			if ($type == "object") {
+				$value = get_object_vars($value);
+			} 
+			foreach ($value as $k=>$v) {
+				$esc_key = sajax_esc($k);
+				if (is_numeric($k)) 
+					$s .= "$k: " . sajax_get_js_repr($v) . ", ";
+				else
+					$s .= "\"$esc_key\": " . sajax_get_js_repr($v) . ", ";
+			}
+			if (count($value))
+				$s = substr($s, 0, -2);
+			return $s . " }";
+		} 
+		else {
+			$esc_val = sajax_esc($value);
+			$s = "'$esc_val'";
+			return $s;
+		}
+	}
 
 	function sajax_handle_client_request() {
 		global $sajax_export_list;
@@ -39,6 +89,8 @@ if (!isset($SAJAX_INCLUDED)) {
 		if (empty($mode)) 
 			return;
 
+		$target = "";
+		
 		if ($mode == "get") {
 			// Bust cache in the head
 			header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
@@ -65,7 +117,7 @@ if (!isset($SAJAX_INCLUDED)) {
 		else {
 			echo "+:";
 			$result = call_user_func_array($func_name, $args);
-			echo $result;
+			echo "var res = " . trim(sajax_get_js_repr($result)) . "; res;";
 		}
 		exit;
 	}
@@ -74,9 +126,10 @@ if (!isset($SAJAX_INCLUDED)) {
 		global $sajax_debug_mode;
 		global $sajax_request_type;
 		global $sajax_remote_uri;
+		global $sajax_failure_redirect;
 		
 		$t = strtoupper($sajax_request_type);
-		if ($t != "GET" && $t != "POST") 
+		if ($t != "" && $t != "GET" && $t != "POST") 
 			return "// Invalid type: $t.. \n\n";
 		
 		ob_start();
@@ -86,75 +139,148 @@ if (!isset($SAJAX_INCLUDED)) {
 		// (c) copyright 2005 modernmethod, inc
 		var sajax_debug_mode = <?php echo $sajax_debug_mode ? "true" : "false"; ?>;
 		var sajax_request_type = "<?php echo $t; ?>";
+		var sajax_target_id = "";
+		var sajax_failure_redirect = "<?php echo $sajax_failure_redirect; ?>";
 		
 		function sajax_debug(text) {
 			if (sajax_debug_mode)
-				alert("RSD: " + text)
+				alert(text);
 		}
+		
  		function sajax_init_object() {
  			sajax_debug("sajax_init_object() called..")
  			
  			var A;
-			try {
-				A=new ActiveXObject("Msxml2.XMLHTTP");
-			} catch (e) {
+ 			
+ 			var msxmlhttp = new Array(
+				'Msxml2.XMLHTTP.5.0',
+				'Msxml2.XMLHTTP.4.0',
+				'Msxml2.XMLHTTP.3.0',
+				'Msxml2.XMLHTTP',
+				'Microsoft.XMLHTTP');
+			for (var i = 0; i < msxmlhttp.length; i++) {
 				try {
-					A=new ActiveXObject("Microsoft.XMLHTTP");
-				} catch (oc) {
-					A=null;
+					A = new ActiveXObject(msxmlhttp[i]);
+				} catch (e) {
+					A = null;
 				}
 			}
+ 			
 			if(!A && typeof XMLHttpRequest != "undefined")
 				A = new XMLHttpRequest();
 			if (!A)
 				sajax_debug("Could not create connection object.");
 			return A;
 		}
+		
+		var sajax_requests = new Array();
+		
+		function sajax_cancel() {
+			for (var i = 0; i < sajax_requests.length; i++) 
+				sajax_requests[i].abort();
+		}
+		
 		function sajax_do_call(func_name, args) {
 			var i, x, n;
 			var uri;
 			var post_data;
+			var target_id;
+			
+			sajax_debug("in sajax_do_call().." + sajax_request_type + "/" + sajax_target_id);
+			target_id = sajax_target_id;
+			if (typeof(sajax_request_type) == "undefined" || sajax_request_type == "") 
+				sajax_request_type = "GET";
 			
 			uri = "<?php echo $sajax_remote_uri; ?>";
 			if (sajax_request_type == "GET") {
+			
 				if (uri.indexOf("?") == -1) 
-					uri = uri + "?rs=" + escape(func_name);
+					uri += "?rs=" + escape(func_name);
 				else
-					uri = uri + "&rs=" + escape(func_name);
+					uri += "&rs=" + escape(func_name);
+				uri += "&rst=" + escape(sajax_target_id);
+				uri += "&rsrnd=" + new Date().getTime();
+				
 				for (i = 0; i < args.length-1; i++) 
-					uri = uri + "&rsargs[]=" + escape(args[i]);
-				uri = uri + "&rsrnd=" + new Date().getTime();
+					uri += "&rsargs[]=" + escape(args[i]);
+
 				post_data = null;
-			} else {
+			} 
+			else if (sajax_request_type == "POST") {
 				post_data = "rs=" + escape(func_name);
+				post_data += "&rst=" + escape(sajax_target_id);
+				post_data += "&rsrnd=" + new Date().getTime();
+				
 				for (i = 0; i < args.length-1; i++) 
 					post_data = post_data + "&rsargs[]=" + escape(args[i]);
 			}
+			else {
+				alert("Illegal request type: " + sajax_request_type);
+			}
 			
 			x = sajax_init_object();
-			x.open(sajax_request_type, uri, true);
-			if (sajax_request_type == "POST") {
-				x.setRequestHeader("Method", "POST " + uri + " HTTP/1.1");
-				x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-			}
-			x.onreadystatechange = function() {
-				if (x.readyState != 4) 
-					return;
-				sajax_debug("received " + x.responseText);
+			if (x == null) {
+				if (sajax_failure_redirect != "") {
+					location.href = sajax_failure_redirect;
+					return false;
+				} else {
+					sajax_debug("NULL sajax object for user agent:\n" + navigator.userAgent);
+					return false;
+				}
+			} else {
+				x.open(sajax_request_type, uri, true);
+				// window.open(uri);
 				
-				var status;
-				var data;
-				status = x.responseText.charAt(0);
-				data = x.responseText.substring(2);
-				if (status == "-") 
-					alert("Error: " + data);
-				else  
-					args[args.length-1](data);
+				sajax_requests[sajax_requests.length] = x;
+				
+				if (sajax_request_type == "POST") {
+					x.setRequestHeader("Method", "POST " + uri + " HTTP/1.1");
+					x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				}
+			
+				x.onreadystatechange = function() {
+					if (x.readyState != 4) 
+						return;
+
+					sajax_debug("received " + x.responseText);
+				
+					var status;
+					var data;
+					var txt = x.responseText.replace(/^\s*|\s*$/g,"");
+					status = txt.charAt(0);
+					data = txt.substring(2);
+
+					if (status == "") {
+						// let's just assume this is a pre-response bailout and let it slide for now
+					} else if (status == "-") 
+						alert("Error: " + data);
+					else {
+						if (target_id != "") 
+							document.getElementById(target_id).innerHTML = eval(data);
+						else {
+							try {
+								var callback;
+								var extra_data = false;
+								if (typeof args[args.length-1] == "object") {
+									callback = args[args.length-1].callback;
+									extra_data = args[args.length-1].extra_data;
+								} else {
+									callback = args[args.length-1];
+								}
+								callback(eval(data), extra_data);
+							} catch (e) {
+								sajax_debug("Caught error " + e + ": Could not eval " + data );
+							}
+						}
+					}
+				}
 			}
-			x.send(post_data);
+			
 			sajax_debug(func_name + " uri = " + uri + "/post = " + post_data);
+			x.send(post_data);
 			sajax_debug(func_name + " waiting..");
 			delete x;
+			return true;
 		}
 		
 		<?php
@@ -170,7 +296,11 @@ if (!isset($SAJAX_INCLUDED)) {
 	// javascript escape a value
 	function sajax_esc($val)
 	{
-		return str_replace('"', '\\\\"', $val);
+		$val = str_replace("\\", "\\\\", $val);
+		$val = str_replace("\r", "\\r", $val);
+		$val = str_replace("\n", "\\n", $val);
+		$val = str_replace("'", "\\'", $val);
+		return str_replace('"', '\\"', $val);
 	}
 
 	function sajax_get_one_stub($func_name) {
