5972d4e5a708df42c70d3271d5bdebbc2a6374fe
trisweb <trisweb@46129fc4-274e-7f48-8cfa-5da4c607681c>
2006-05-01 08:04:22 +0000

    Layout fixes for the testing theme.
    git-svn-id: http://www.zenphoto.org/svn/trunk@205 46129fc4-274e-7f48-8cfa-5da4c607681c

--- themes/testing/album.php
+++ themes/testing/album.php
@@ -31,10 +31,9 @@
   
   <?php printPageNav("&laquo; prev", "|", "next &raquo;"); ?>
 
-  
+  <div id="credit"><?php printAdminLink('Admin', '', ' | '); ?>Powered by <a href="http://www.zenphoto.org" title="A simpler web photo album">zenphoto</a><br />
+  <?php echo round((array_sum(explode(" ",microtime())) - $startTime),4).' Seconds</strong>'; ?></div>
 </div>
-
-<?php echo round((array_sum(explode(" ",microtime())) - $startTime),4).' Seconds</strong>'; ?>
 
 </body>
 </html>
