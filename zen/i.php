a0e5e860fa9b2188c0c7bd74527b8148e794c4d4
trisweb <trisweb@46129fc4-274e-7f48-8cfa-5da4c607681c>
2007-02-13 19:55:00 +0000

    Decreased default sharpening juuuuust a bit.
    git-svn-id: http://www.zenphoto.org/svn/trunk@362 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zen/i.php
+++ zen/i.php
@@ -194,7 +194,7 @@ if ($process) {
     }
     
     if ($thumb && $sharpenthumbs) {
-      unsharp_mask($newim, 50, 0.5, 3);
+      unsharp_mask($newim, 40, 0.5, 3);
     }
 
     // Create the cached file (with lots of compatibility)...
