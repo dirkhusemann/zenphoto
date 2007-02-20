aa071c40d41c3504c9a9ea3c523e8aeedaaed9d6
trisweb <trisweb@46129fc4-274e-7f48-8cfa-5da4c607681c>
2007-02-20 08:03:14 +0000

    Fixed redirect for query string paths without a .php.
    git-svn-id: http://www.zenphoto.org/svn/trunk@372 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zen/functions.php
+++ zen/functions.php
@@ -80,10 +80,12 @@ function truncate_string($string, $length) {
 function is_query_request() {
   $path = urldecode(substr($_SERVER['REQUEST_URI'], strlen(WEBPATH)+1));
   $qpos = strpos($path, '?');
+  if ($qpos !== false && strpos($path, '/?') !== false)
+    return true;
   if ($qpos !== false)
     $path = substr($path, 0, $qpos);
-  if (strpos($path, '.php') === false) return false;
-  return true;
+  if (strpos($path, '.php') !== false) return true;
+  return false;
 }
 
 
