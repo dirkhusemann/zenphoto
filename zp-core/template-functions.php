2aac09edf9a1626c9cefa419454510ce5d5f1900
aitf311 <aitf311@46129fc4-274e-7f48-8cfa-5da4c607681c>
2007-11-20 02:17:04 +0000

    retreat on #299 fix!
    git-svn-id: http://www.zenphoto.org/svn/trunk@840 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/template-functions.php
+++ zp-core/template-functions.php
@@ -1657,19 +1657,18 @@ function getAlbumId() {
 }
 
 /* Returns the ID of all sub-albums, relative to the current album. If $_zp_current_album is not set, it'll return null. */
-function getRandomImagesAlbum() {		
-  $images = array();
-  $subIDs = getAllSubAlbumIDs($rootAlbum);
-  if($subIDs == null) {return null;}; //no subdirs avaliable
-  foreach ($subIDs as $ID) {		
-    $query = 'SELECT `id` , `albumid` , `filename` , `title` FROM '.prefix('images').' WHERE `albumid` = "'. $ID['id'] .'"'; 		
-    $images = array_merge($images, query_full_array($query)); 	
+function getAllSubAlbumIDs($albumfolder='') {	
+  global $_zp_current_album;
+  if (empty($albumfolder)) {
+    if (isset($_zp_current_album)) { 
+	  $albumfolder = $_zp_current_album->getFolder();
+	} else {
+	  return null; 
+	}	
   }
-  if(count($images) < 1){return null;}; //no images avaliable in _any_ subdirectory
-  $randomImage = $images[array_rand($images)];
-  $folderPath = query_single_row("SELECT `folder` FROM " .prefix('albums'). " WHERE id = '" .$randomImage['albumid']. "'"  ); 	
-  $image = new Image(new Album(new Gallery(), $folderPath['folder']), $randomImage['filename']);	
-  return $image;
+  $query = "SELECT `id` FROM " . prefix('albums') . " WHERE `folder` LIKE '" . $albumfolder . "%'"; 
+  $subIDs = query_full_array($query);
+  return $subIDs; 
 }
 
 function hitcounter() {
