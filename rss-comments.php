9d7ee90c87c9fb7717f48a8883dc64e56ad693b5
aitf311 <aitf311@46129fc4-274e-7f48-8cfa-5da4c607681c>
2007-11-21 14:42:43 +0000

    mod_rewrite fix
    git-svn-id: http://www.zenphoto.org/svn/trunk@849 46129fc4-274e-7f48-8cfa-5da4c607681c

--- rss-comments.php
+++ rss-comments.php
@@ -5,9 +5,9 @@ require_once(ZENFOLDER . "/template-functions.php");
 $themepath = 'themes';
 
 if(getOption('mod_rewrite')) { 
-  $albumpath = "/"; $imagepath = "/"; 
+  $albumpath = "/"; $imagepath = "/"; $modrewritesuffix = getOption('mod_rewrite_image_suffix');
 } else { 
-  $albumpath = "/index.php?album="; $imagepath = "&image="; 
+  $albumpath = "/index.php?album="; $imagepath = "&image="; $modrewritesuffix = "";
 }
 $items = getOption('feed_items'); // # of Items displayed on the feed
 ?>
@@ -25,12 +25,13 @@ $items = getOption('feed_items'); // # of Items displayed on the feed
 <webMaster><?php echo getOption('admin_email'); ?></webMaster>
 <?php
 db_connect();
-$comments = query_full_array("SELECT c.id, i.title, i.filename, a.folder, a.title AS albumtitle, c.name, c.website," . " c.date, c.comment FROM ".prefix('comments')." AS c, ".prefix('images')." AS i, ".prefix('albums')." AS a " . " WHERE c.imageid = i.id AND i.albumid = a.id ORDER BY c.id DESC LIMIT ".$items;);
+$comments = query_full_array("SELECT c.id, i.title, i.filename, a.folder, a.title AS albumtitle, c.name, c.website," . " c.date, c.comment FROM ".prefix('comments')." AS c, ".prefix('images')." AS i, ".prefix('albums')." AS a " . " WHERE c.imageid = i.id AND i.albumid = a.id ORDER BY c.id DESC LIMIT $items");
 foreach ($comments as $comment)
 {
 $author = $comment['name'];
 $album = $comment['folder'];
 $image = $comment['filename'];
+$date = $comment['date'];
 $albumtitle = $comment['albumtitle'];
 if ($comment['title'] == "") $title = $image; else $title = $comment['title'];
 $website = $comment['website'];
@@ -38,10 +39,12 @@ $comment = truncate_string($comment['comment'], 123);
 ?>
 <item>
 <title><?php echo $albumtitle.": ".$title." by ".$author; ?></title>
-<link><?php echo '<![CDATA[http://'.$_SERVER['HTTP_HOST'].WEBPATH.$albumpath.$album.$imagepath.$image.']]>';?></link>
+<link><?php echo '<![CDATA[http://'.$_SERVER['HTTP_HOST'].WEBPATH.$albumpath.$album.$imagepath.$image.$modrewritesuffix.']]>';?></link>
 <dc:creator><?php echo $author; ?></dc:creator>
 <description><?php echo $comment; ?></description>
-<guid><?php echo '<![CDATA[http://'.$_SERVER['HTTP_HOST'].WEBPATH.$albumpath.$album.$imagepath.$image.']]>';?></guid>
+<category><?php echo $albumtitle; ?></category>
+<guid><?php echo '<![CDATA[http://'.$_SERVER['HTTP_HOST'].WEBPATH.$albumpath.$album.$imagepath.$image.$modrewritesuffix.']]>';?></guid>
+<pubDate><?php echo $date; ?></pubDate> 
 </item>
 <?php } ?>
 </channel>
