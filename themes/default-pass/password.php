6ea347afa6c02afd388d016642193684656e3a4d
aitf311 <aitf311@46129fc4-274e-7f48-8cfa-5da4c607681c>
2007-11-24 04:27:35 +0000

    no need for default-pass, every theme can be protected now, and better than this implementation
    git-svn-id: http://www.zenphoto.org/svn/trunk@857 46129fc4-274e-7f48-8cfa-5da4c607681c

--- themes/default-pass/password.php
+++
@@ -1,5 +0,0 @@
-<form name="form" method="post" action="<?php echo getPageURL(getCurrentPage()); ?>">
-<label for="newwws_pw">Password:</label>
-<input type="password" title="Enter your password" name="newwws_pw" />
-<input type="submit" name="Submit" value="Login" />
-</form>
