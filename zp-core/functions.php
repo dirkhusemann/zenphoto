cd8aa41a54b8fbf11882e2e7ae76953d58c21f37
buildscript <buildscript@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-02-24 07:00:16 +0000

    Nightly script: build tag updated to 1179
    git-svn-id: http://www.zenphoto.org/svn/trunk@1179 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/functions.php
+++ zp-core/functions.php
@@ -1,6 +1,6 @@
 <?php
 define('ZENPHOTO_VERSION', '1.1.4');
-define('ZENPHOTO_RELEASE', 1172);
+define('ZENPHOTO_RELEASE', 1179);
 define('SAFE_GLOB', false);
 define('CHMOD_VALUE', 0777);
 if (!defined('ZENFOLDER')) { define('ZENFOLDER', 'zp-core'); }
