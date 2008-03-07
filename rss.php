f131387ce4d533d48510eda36823dd809f51dce3
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-03-07 17:28:08 +0000

    RSS validation
    git-svn-id: http://www.zenphoto.org/svn/trunk@1256 46129fc4-274e-7f48-8cfa-5da4c607681c

--- rss.php
+++ rss.php
@@ -4,6 +4,19 @@ header('Content-Type: application/xml');
 require_once(ZENFOLDER . "/template-functions.php");
 $themepath = 'themes';
 
+function fixRSSDate($bad_date) {
+	$rval = FALSE;
+	$parts = explode(" ", $bad_date);
+	$date = $parts[0];
+	$time = $parts[1];
+	$date_parts = explode("-", $date);
+	$year = $date_parts[0];
+	$month = $date_parts[2];
+	$day = $date_parts[1];
+	$rval = date("r",strtotime("$day/$month/$year $time"));
+	return $rval;
+}
+
 $albumnr = sanitize_numeric($_GET['albumnr']);
 $albumname = sanitize($_GET['albumname'], true);
 $host = htmlentities($_SERVER["HTTP_HOST"], ENT_QUOTES, 'UTF-8');
@@ -16,31 +29,30 @@ foreach($albumscheck as $albumcheck) {
 		$passwordcheck = $passwordcheck.$albumpasswordcheck;
 	} 
 }
-
 if ($albumname != "") { $albumname = " - for album: ".$albumname; }
-if(getOption('mod_rewrite'))
- { $albumpath = "/"; $imagepath = "/"; $modrewritesuffix = getOption('mod_rewrite_image_suffix'); }
-else
- { $albumpath = "/index.php?album="; $imagepath = "&image="; $modrewritesuffix = ""; }
+if(getOption('mod_rewrite')) {
+	$albumpath = "/"; $imagepath = "/"; $modrewritesuffix = getOption('mod_rewrite_image_suffix');
+} else  {
+	$albumpath = "/index.php?album="; $imagepath = "&image="; $modrewritesuffix = ""; }
 
 ?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 <title><?php echo getOption('gallery_title'); ?><?php echo $albumname; ?></title>
 <link><?php echo "http://".$host.WEBPATH; ?></link>
+<atom:link href="http://<?php echo $host.WEBPATH; ?>/rss.php" rel="self" type="application/rss+xml" />
 <description><?php echo getOption('gallery_title'); ?></description>
 <language>en-us</language>
 <pubDate><?php echo date("r", time()); ?></pubDate>
 <lastBuildDate><?php echo date("r", time()); ?></lastBuildDate>
 <docs>http://blogs.law.harvard.edu/tech/rss</docs>
-<generator>Acrylian's ZenPhoto Album RSS Generator based on Alenônimo's ZenPhoto RSS Generator which is based on ThinkDreams' Generator</generator>
+<generator>ZenPhoto RSS Generator</generator>
 <?php
 	$admin = array_shift(getAdministrators());
 	$adminname = $admin['name'];
 ?>
-
-<managingEditor><?php echo $adminname; ?></managingEditor>
-<webMaster><?php echo $adminname; ?></webMaster>
+<managingEditor><?php echo "$admin[email] ($adminname)"; ?></managingEditor>
+<webMaster><?php echo "$admin[email] ($adminname)"; ?></webMaster>
 <?php 
 $s = 240; // uncropped image size
 $items = getOption('feed_items'); // # of Items displayed on the feed
@@ -52,7 +64,6 @@ if (is_numeric($albumnr) && $albumnr != "") {
 } else {
 	$albumWhere = "";
 }
-
 
 $result = query_full_array("SELECT images.albumid, images.date AS date, images.filename AS filename, images.title AS title, " .
  														"albums.folder AS folder, albums.title AS albumtitle, images.show, albums.show, albums.password FROM " . 
@@ -76,7 +87,7 @@ foreach ($result as $images) {
 	<description><?php echo '<![CDATA[<a title="'.$images['title'].' in '.$images['albumtitle'].'" href="http://'.$host.WEBPATH.$albumpath.$images['folder'].$imagepath.$images['filename'].$modrewritesuffix.'"><img border="0" src="http://'.$host.WEBPATH.'/'.ZENFOLDER.'/i.php?a='.$images['folder'].'&i='.$images['filename'].'&s='.$s.'" alt="'. $images['title'] .'"></a>' . $images['desc'] . ']]>';?> <?php if($exif['datetime']) { echo '<![CDATA[Date: ' . $exif['datetime'] . ']]>'; } ?></description>
 		<category><?php echo $images['title']; ?></category>
 	<guid><?php echo '<![CDATA[http://'.$host.WEBPATH.$albumpath.$images['folder'].$imagepath.$images['filename'].$modrewritesuffix. ']]>';?></guid>
-	<pubDate><?php echo $images['date']; ?></pubDate> 
+	<pubDate><?php echo fixRSSDate($images['date']); ?></pubDate> 
 </item>
 <?php } ?>
 </channel>
