0da93dca026bd753d74a58f7d29e2f56df02d165
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-05-13 16:22:07 +0000

    updates to image_album_statistics.php
    git-svn-id: http://www.zenphoto.org/svn/trunk@1533 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/plugins/image_album_statistics.php
+++ zp-core/plugins/image_album_statistics.php
@@ -7,7 +7,7 @@
 
 $plugin_description = gettext("Functions that provide various statistics about images and albums in the gallery.");
 $plugin_author = "Malte Müller (acrylian), Stephen Billard (sbillard)";
-$plugin_version = '1.0.2';
+$plugin_version = '1.0.3';
 $plugin_URL = "http://www.zenphoto.org/documentation/zenphoto/_plugins---image_album_statistics.php.html";
 
 /**
@@ -17,6 +17,7 @@ $plugin_URL = "http://www.zenphoto.org/documentation/zenphoto/_plugins---image_a
  * @param string $option "popular" for the most popular albums,
  *     "latest" for the latest uploaded, "mostrated" for the most voted,
  *     "toprated" for the best voted
+ * 		 "latestupdated" for the latest updated
  * @return string
  */
 function getAlbumStatistic($number=5, $option) {
@@ -43,9 +44,40 @@ function getAlbumStatistic($number=5, $option) {
 			$sortorder = "total_votes"; break;
 		case "toprated":
 			$sortorder = "(total_value/total_votes)"; break;
+		case "latestupdated":
+			// get all albums
+			$allalbums = query_full_array("SELECT id, title, folder, thumb FROM " . prefix('albums'). $albumWhere);
+			$latestimages = array();
+
+			// get latest images of each album
+			foreach($allalbums as $album) {
+				$image = query_single_row("SELECT id, albumid FROM " . prefix('images'). " WHERE albumid = ".$album['id'] . " AND `show` = 1 ORDER BY id DESC");
+				array_push($latestimages, $image);
+			}
+			// sort latest image by mtime
+			arsort($latestimages);
+			//print_r($latestimages);
+			$updatedalbums = array();
+			$count = 0;
+			foreach($latestimages as $latestimage) {
+				$count++;
+				foreach($allalbums as $album) {
+					if($album['id'] === $latestimage['albumid']) {
+						array_push($updatedalbums,$album);
+					}
+				}
+				if($count === $number) {
+					break;
+				}
+			}
+			break;
 	}
-	$albums = query("SELECT id, title, folder, thumb FROM " . prefix('albums') . $albumWhere . " ORDER BY ".$sortorder." DESC LIMIT $number");
-	return $albums;
+	if($option === "latestupdated") {
+		return $updatedalbums;
+	} else {
+		$albums = query("SELECT id, title, folder, thumb FROM " . prefix('albums') . $albumWhere . " ORDER BY ".$sortorder." DESC LIMIT $number");
+		return $albums;
+	}
 }
 
 /**
@@ -58,6 +90,7 @@ function getAlbumStatistic($number=5, $option) {
  *                  "latest" for the latest uploaded,
  *                  "mostrated" for the most voted,
  *                  "toprated" for the best voted
+ * 									"latestupdated" for the latest updated
  * @param bool $showtitle if the album title should be shown
  * @param bool $showdate if the album date should be shown
  * @param bool $showdesc if the album desc should be shown
@@ -66,31 +99,43 @@ function getAlbumStatistic($number=5, $option) {
 function printAlbumStatistic($number, $option, $showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
 	$albums = getAlbumStatistic($number, $option);
 	echo "\n<div id=\"".$option."_album\">\n";
-	if (getOption('mod_rewrite'))
-	{ $albumlinkpath = WEBPATH."/";
-	} else {
-		$albumlinkpath = "index.php?album=";
-	}
+	$albumpath = rewrite_path("/", "index.php?album=");
 	echo "<ul>";
-	$gallery = new Gallery();
-	while ($album = mysql_fetch_array($albums)) {
-		$tempalbum = new Album($gallery, $album['folder']);
-		echo "<li><a href=\"".pathurlencode($tempalbum->name)."\" title=\"" . $tempalbum->getTitle() . "\">\n";
+	if ($option === "latestupdated") { // needs a "normal" array
+		foreach($albums as $album) {
+			printAlbumStatisticItem($album, $option,$showtitle, $showdate, $showdesc, $desclength);
+		}
+	} else {
+		while ($album = mysql_fetch_array($albums)) { // needs a mysql array
+			printAlbumStatisticItem($album, $option,$showtitle, $showdate, $showdesc, $desclength);
+		}
+		echo "</ul></div>\n";
+	}
+}
+
+/**
+ * A helper function that only prints a item of the loop within printAlbumStatistic()
+ * Not for standalone use.
+ *
+ * @param array $album the array that getAlbumsStatistic() submitted
+ */
+function printAlbumStatisticItem($album, $showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	global $_zp_gallery;
+	$tempalbum = new Album($_zp_gallery, $album['folder']);
+		echo "<li><a href=\"".$albumpath.pathurlencode($tempalbum->name)."\" title=\"" . $tempalbum->getTitle() . "\">\n";
 		echo "<img src=\"".$tempalbum->getAlbumThumb()."\"></a>\n<br />";
 		if($showtitle) {
-			echo "<h3><a href=\"".pathurlencode($tempalbum->name)."\" title=\"" . $tempalbum->getTitle() . "\">\n";
+			echo "<h3><a href=\"".$albumpath.pathurlencode($tempalbum->name)."\" title=\"" . $tempalbum->getTitle() . "\">\n";
 			echo $tempalbum->getTitle()."</a>\n";
 		}
 		echo "</h3>";
 		if($showdate) {
-			echo "<p>". zpFormattedDate(getOption('date_format'),strtotime($tempalbum->getDateTime()))."</p>";
+			echo "<p>Date: ". zpFormattedDate(getOption('date_format'),strtotime($tempalbum->getDateTime()))."</p>";
 		}
 		if($showdesc) {
 			echo "<p>".my_truncate_string($tempalbum->getDesc(), $desclength)."</p>";
 		}
 		echo "</li>";
-	}
-	echo "</ul></div>\n";
 }
 
 /**
@@ -98,8 +143,8 @@ function printAlbumStatistic($number, $option, $showtitle=false, $showdate=false
  *
  * @param string $number the number of albums to get
  */
-function printPopularAlbums($number=5) {
-	printAlbumStatistic($number,"popular");
+function printPopularAlbums($number=5,$showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printAlbumStatistic($number,"popular",$showtitle, $showdate, $showdesc, $desclength);
 }
 
 /**
@@ -107,8 +152,8 @@ function printPopularAlbums($number=5) {
  *
  * @param string $number the number of albums to get
  */
-function printLatestAlbums($number=5) {
-	printAlbumStatistic($number,"latest");
+function printLatestAlbums($number=5,$showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printAlbumStatistic($number,"latest",$showtitle, $showdate, $showdesc, $desclength);
 }
 
 /**
@@ -116,8 +161,8 @@ function printLatestAlbums($number=5) {
  *
  * @param string $number the number of albums to get
  */
-function printMostRatedAlbums($number=5) {
-	printAlbumStatistic($number,"mostrated");
+function printMostRatedAlbums($number=5,$showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printAlbumStatistic($number,"mostrated",$showtitle, $showdate, $showdesc, $desclength);
 }
 
 /**
@@ -125,8 +170,17 @@ function printMostRatedAlbums($number=5) {
  *
  * @param string $number the number of albums to get
  */
-function printTopRatedAlbums($number=5) {
-	printAlbumStatistic($number,"toprated");
+function printTopRatedAlbums($number=5,$showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printAlbumStatistic($number,"toprated",$showtitle, $showdate, $showdesc, $desclength);
+}
+
+/**
+ * Prints the top voted albums
+ *
+ * @param string $number the number of albums to get
+ */
+function printLatestUpdatedAlbums($number=5,$showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printAlbumStatistic($number,"latestupdated",$showtitle, $showdate, $showdesc, $desclength);
 }
 
 /**
@@ -176,17 +230,15 @@ function getImageStatistic($number, $option, $album='') {
 			$sortorder = "(images.total_value/images.total_votes)"; break;
 	}
 	$imageArray = array();
-	$images = query_full_array("SELECT images.albumid, images.filename AS filename, images.title AS title, " .
+	$images = query_full_array("SELECT images.albumid, images.filename AS filename, images.mtime as mtime, images.title AS title, " .
  														"albums.folder AS folder, images.show, albums.show, albums.password FROM " .
 	prefix('images') . " AS images, " . prefix('albums') . " AS albums " .
 															" WHERE ".$specificalbum."images.albumid = albums.id " . $imageWhere . $albumWhere .
 															" AND albums.folder != ''".
 															" ORDER BY ".$sortorder." DESC LIMIT $number");
 	foreach ($images as $imagerow) {
-
 		$filename = $imagerow['filename'];
 		$albumfolder = $imagerow['folder'];
-
 		$desc = $imagerow['title'];
 		// Album is set as a reference, so we can't re-assign to the same variable!
 		$image = new Image(new Album($_zp_gallery, $albumfolder), $filename);
@@ -237,26 +289,27 @@ function printImageStatistic($number, $option, $album='', $showtitle=false, $sho
  * @param string $number the number of images to get
  * @param string $album title of an specific album
  */
-function printPopularImages($number=5, $album='') {
-	printImageStatistic($number, "popular",$album);
+function printPopularImages($number=5, $album='', $showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printImageStatistic($number, "popular",$album, $showtitle, $showdate, $showdesc, $desclength);
 }
 
 /**
- * Prints the most rated images
- *
- * @param string $number the number of images to get
+* Prints the n top rated images
+*
+* @param int $number The number if images desired
  */
-function printMostRatedImages($number=5) {
-	printImageStatistic($number,"mostrated");
+function printTopRatedImages($number=5, $showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printImageStatistic($number, "toprated", $showtitle, $showdate, $showdesc, $desclength);
 }
 
+
 /**
- * Prints the top voted images
+ * Prints the n most rated images
  *
- * @param string $number the number of images to get
+ * @param int $number The number if images desired
  */
-function printTopRatedImages($number=5) {
-	printImageStatistic($number,"toprated");
+function printMostRatedImages($number=5, $showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printImageStatistic($number, "mostrated", $showtitle, $showdate, $showdesc, $desclength);
 }
 
 /**
@@ -265,8 +318,8 @@ function printTopRatedImages($number=5) {
  * @param string $number the number of images to get
  * @param string $album title of an specific album
  */
-function printLatestImages($number=5, $album='') {
-	printImageStatistic($number, "latest", $album);
+function printLatestImages($number=5, $album='', $showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printImageStatistic($number, "latest", $album, $showtitle, $showdate, $showdesc, $desclength);
 }
 
 /**
@@ -275,8 +328,8 @@ function printLatestImages($number=5, $album='') {
  * @param string $number the number of images to get
  * @param string $album title of an specific album
  */
-function printLatestImagesByDate($number=5, $album='') {
-	printImageStatistic($number, "latest-date", $album);
+function printLatestImagesByDate($number=5, $album='', $showtitle=false, $showdate=false, $showdesc=false, $desclength=40) {
+	printImageStatistic($number, "latest-date", $album, $showtitle, $showdate, $showdesc, $desclength);
 }
 
 ?>
