8385da0360910f5a7d1c5c550239953580737c22
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-05-26 19:05:35 +0000

    Fixes gettext typo of example theme's theme_description.php
    git-svn-id: http://www.zenphoto.org/svn/trunk@1598 46129fc4-274e-7f48-8cfa-5da4c607681c

--- themes/example/theme_description.php
+++ themes/example/theme_description.php
@@ -4,5 +4,5 @@ $theme_description['name'] = 'Example Theme';
 $theme_description['author'] = 'Tristan Harward';
 $theme_description['version'] = '1.5';
 $theme_description['date'] = '11/05/2007';
-$theme_description['desc'] = Gettext("This theme was designed to show off many of ZenPhoto's features. It includes many template tags and doesn't look half bad. Also a great example if you want to make your own theme or integrate ZenPhoto into a blog.");
+$theme_description['desc'] = gettext("This theme was designed to show off many of ZenPhoto's features. It includes many template tags and doesn't look half bad. Also a great example if you want to make your own theme or integrate ZenPhoto into a blog.");
 ?>
