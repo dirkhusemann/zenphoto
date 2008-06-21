01d4841d56250659114704f4525ff1d192fd386b
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-06-21 17:05:05 +0000

    jQuery tag suggestion plugin
    git-svn-id: http://www.zenphoto.org/svn/trunk@1755 46129fc4-274e-7f48-8cfa-5da4c607681c

---
+++ zp-core/plugins/tag_suggest.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * tag suggest plugin draft based on Remy Sharp's jQuery Tag Suggestion plugin
+ * 
+ * Just activate the plugin and the feature is available on the theme's search field.
+ *  
+ */
+
+$plugin_description = gettext("Enables jQuery tag suggestions on the search field. Just activate the plugin and the feature is available on the theme's search field.");
+$plugin_author = "Malte Müller (acrylian), Stephen Billard (sbillard) - an adaption of Remy Sharp's <a href='http://remysharp.com/2007/12/28/jquery-tag-suggestion/ '>jQuery Tag Suggestion</a>";
+$plugin_version = '1.0.0';
+$plugin_URL = "http://www.zenphoto.org/documentation/zenphoto/_plugins---tag_suggest.php.html";
+
+// register the scripts needed
+addPluginScript('<script type="text/javascript" src="' . WEBPATH . '/' . ZENFOLDER . '/plugins/tag_suggest/tag.js"></script>');
+addPluginScript('<link rel="stylesheet" href="' . WEBPATH . '/' . ZENFOLDER . '/plugins/tag_suggest/tag.css" />');
+
+$taglist = getAllTagsUnique();
+$c = 0;
+$list = '';
+foreach ($taglist AS $tag) {
+	if ($c>0) $list .= ',';
+	$c++;
+	$list .= '"'.$tag.'"';
+}
+$js = '<script type="text/javascript">'.
+				'$(function () {'.
+					"$('#search_input').tagSuggest({".
+						'tags: ['.$list.']'.
+					'});'.
+				'});'.
+			'</script>';
+
+addPluginScript($js);
+?>
