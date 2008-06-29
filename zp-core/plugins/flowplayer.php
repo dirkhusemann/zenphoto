dddc2cfcb8a00b327d1db540304bb124b26683f0
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-06-29 14:43:44 +0000

    Adds "autoplay" and color options to flow player plugin.
    git-svn-id: http://www.zenphoto.org/svn/trunk@1824 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/plugins/flowplayer.php
+++ zp-core/plugins/flowplayer.php
@@ -8,7 +8,7 @@
 
 $plugin_description = gettext("Enable <strong>flowplayer</strong> to handle multimedia files. IMPORTANT: Only one multimedia player plugin can be enabled at the time. <br> Please see <a href='http://flowplayer.org'>flowplayer.org</a> for more info about the player and its licence.");
 $plugin_author = "Malte Müller (acrylian), Stephen Billard (sbillard)";
-$plugin_version = '1.0.1.3';
+$plugin_version = '1.0.1.5';
 $plugin_URL = "http://www.zenphoto.org/documentation/zenphoto/_plugins---flowplayer.php.html";
 $option_interface = new flowplayer();
 $_zp_flash_player = $option_interface; // claim to be the flash player.
@@ -24,7 +24,9 @@ class flowplayer {
 	function flowplayer() {
 		setOptionDefault('flow_player_width', '320');
 		setOptionDefault('flow_player_height', '240');
-
+		setOptionDefault('flow_player_controlbarbackgroundcolor', '0x567890');
+		setOptionDefault('flow_player_controlsareabordercolor', '0x567890');
+		setOptionDefault('flow_player_autoplay', '');
 	}
 
 
@@ -32,7 +34,13 @@ class flowplayer {
 		return array(	gettext('flow player width') => array('key' => 'flow_player_width', 'type' => 0,
 										'desc' => gettext("Player width (ignored for <em>mp3</em> files.)")),
 		gettext('flow player height') => array('key' => 'flow_player_height', 'type' => 0,
-										'desc' => gettext("Player height (ignored for <em>mp3</em> files.)"))
+										'desc' => gettext("Player height (ignored for <em>mp3</em> files.)")),
+		gettext('Controls background color') => array('key' => 'flow_player_controlbarbackgroundcolor', 'type' => 0,
+										'desc' => gettext("Background color of the controls.")),
+		gettext('Control area border color') => array('key' => 'flow_player_controlsareabordercolor', 'type' => 0,
+										'desc' => gettext("Color of the border of the player controls")),
+		gettext('Autoplay') => array('key' => 'flow_player_autoplay', 'type' => 1,
+										'desc' => gettext("Should the video start automatically. Yes if selected."))
 		);
 	}
 
@@ -43,6 +51,7 @@ class flowplayer {
 	 * @param string $imagetitle the title of the movie [not used by flowplayer]
 	 * 	 */
 	function playerConfig($moviepath='', $imagetitle,$count='') {
+		global $_zp_current_image;
 		if(empty($moviepath)) {
 			$moviepath = getUnprotectedImageURL();
 			$ext = strtolower(strrchr(getUnprotectedImageURL(), "."));
@@ -52,17 +61,23 @@ class flowplayer {
 		if(!empty($count)) {
 			$count = "-".$count;
 		}
+		if(getOption("flow_player_autoplay")) {
+			$autoplay = "true";
+		} else {
+			$autoplay = "false";
+		}
 		if($ext === ".mp3") {
 			echo '
 			<p id="playerContainer'.$count.'"><a href="http://www.adobe.com/go/getflashplayer">'.gettext('Get Flash').'</a> '.gettext('to see this player.').'</p>
 			<script>
 			$("#playerContainer'.$count.'").flashembed({
       	src:\'' . WEBPATH . '/' . ZENFOLDER . '/plugins/flowplayer/FlowPlayerLight.swf\',
-      	width:400, 
+      	width:'.getOption('flow_player_width').', 
       	height:28
     	},
     		{config: {  
-      		autoPlay: false,
+      		autoPlay: \'' . $autoplay . '\',
+      		useNativeFullScreen: true,
 					loop: false,
 					autoBuffering: true,
       		videoFile: \'' . $moviepath . '\',
@@ -80,8 +95,9 @@ class flowplayer {
       	height:'.getOption('flow_player_height').'
     	},
     		{config: {  
-      		autoPlay: false,
-					loop: false,
+      		autoPlay: \'' . $autoplay . '\',
+    			loop: false,
+					controlsOverVideo: "ease",
       		videoFile: \'' . $moviepath . '\',
       		initialScale: \'scale\'
     		}} 
