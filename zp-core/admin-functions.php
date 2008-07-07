31934b0f916f3ca257f581505c5ff59c1c21c06c
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-07-07 16:33:43 +0000

    Fixes missing gettext on browser page header "zenphoto administration".
    git-svn-id: http://www.zenphoto.org/svn/trunk@1884 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/admin-functions.php
+++ zp-core/admin-functions.php
@@ -195,7 +195,7 @@ function printAdminHeader() {
 	echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">";
 	echo "\n<html xmlns=\"http://www.w3.org/1999/xhtml\">";
 	echo "\n<head>";
-	echo "\n  <title>zenphoto administration</title>";
+	echo "\n  <title>".gettext("zenphoto administration")."</title>";
 	echo "\n  <link rel=\"stylesheet\" href=\"admin.css\" type=\"text/css\" />";
 	echo "\n  <link rel=\"stylesheet\" href=\"js/toggleElements.css\" type=\"text/css\" />";
 	echo "\n  <script type=\"text/javascript\" src=\"js/prototype.js\"></script>";
