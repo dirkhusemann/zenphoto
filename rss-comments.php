aa08c45ec98f58b0218b8e35e125235753e46137
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-08-07 15:06:47 +0000

    Fix for #667 comments rss feed.
    git-svn-id: http://www.zenphoto.org/svn/trunk@2105 46129fc4-274e-7f48-8cfa-5da4c607681c

--- rss-comments.php
+++ rss-comments.php
@@ -69,12 +69,12 @@ $items = getOption('feed_items'); // # of Items displayed on the feed
 db_connect();
 $comments_images = query_full_array("SELECT c.id, i.title, i.filename, a.folder, a.title AS albumtitle, c.name, c.type, c.website," 
 . " c.date, c.comment FROM ".prefix('comments')." AS c, ".prefix('images')." AS i, ".prefix('albums')." AS a " 
-. " WHERE c.ownerid = i.id AND i.albumid = a.id AND c.type = 'images'".$passwordcheck1
+. " WHERE i.show = 1 AND c.ownerid = i.id AND i.albumid = a.id AND c.type = 'images'".$passwordcheck1
 . " ORDER BY c.id DESC LIMIT $items");
 
 $comments_albums = query_full_array("SELECT c.id, a.folder, a.title AS albumtitle, c.name, c.type, c.website," 
 . " c.date, c.comment FROM ".prefix('comments')." AS c, ".prefix('albums')." AS a " 
-. " WHERE c.ownerid = a.id AND c.type = 'albums'".$passwordcheck2
+. " WHERE a.show = 1 AND c.ownerid = a.id AND c.type = 'albums'".$passwordcheck2
 . " ORDER BY c.id DESC LIMIT $items"); 
 
 $comments = array();
