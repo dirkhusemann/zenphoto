0d1b1ed966a3d8e47d405f745076dba498115bc6
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-08-07 18:38:58 +0000

    update the styling of the slideshow
    git-svn-id: http://www.zenphoto.org/svn/trunk@2111 46129fc4-274e-7f48-8cfa-5da4c607681c

--- themes/effervescence_plus/slideshow.php
+++ themes/effervescence_plus/slideshow.php
@@ -5,49 +5,58 @@ $themeResult = getTheme($zenCSS, $themeColor, 'effervescence');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/2002/REC-xhtml1-20020801/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+	<?php zenJavascript(); ?>
 <link rel="stylesheet" href="<?php echo $zenCSS ?>" type="text/css" />
 <link rel="stylesheet" href="<?php echo $_zp_themeroot; ?>/slideshow.css" type="text/css" />
 <?php printSlideShowJS(); ?>
 
 </head>
 <body>
-	<div id="subcontent">
-		<div id="submain">	
-			<div id="logo2">
-			<?php printLogo(); ?>
-			</div>
-			<div id="wrapnav">
-				<div id="navbar">
-					<span><?php printHomeLink('', ' | '); ?><a href="<?php echo htmlspecialchars(getGalleryIndexURL());?>" title="<?php echo gettext('Albums Index'); ?>"><?php echo getGalleryTitle();?></a> | 
-					<?php
-					if (!is_null($_zp_current_album)) {
-						printParentBreadcrumb(); 
-						printAlbumBreadcrumb("", " | "); 
-					} else {
-						$search = new SearchEngine();
-						$params = trim(zp_getCookie('zenphoto_image_search_params'));
-						$search->setSearchParams($params);
-						$images = $search->getImages(0);
-						$searchwords = $search->words;
-						$searchdate = $search->dates;
-						$searchfields = $search->fields;
-						$page = $search->page;
-						$returnpath = getSearchURL($searchwords, $searchdate, $searchfields, $page);
-						echo '<a href='.$returnpath.'><em>'.gettext('Search').'</em></a> | ';
-					}
-					?> </span> 
-					Slideshow
-				</div>
-			</div>
-			<div id="content">
- 				<div id="main">
-					<div id="slideshowpage">
-					<?php printSlideShow(false,true); ?>
+	<!-- Wrap Everything -->
+	<div id="main4">
+ 		<div id="main2">
+	
+			<!-- Wrap Header -->
+			<div id="galleryheader">
+				<div id="gallerytitle">
+					<div id="logo2">
+					<?php printLogo(); ?>
 					</div>
+				</div> <!-- gallery title -->
+				
+				<div id="wrapnav">
+					<div id="navbar">
+						<span><?php printHomeLink('', ' | '); ?><a href="<?php echo htmlspecialchars(getGalleryIndexURL());?>" title="<?php echo gettext('Albums Index'); ?>"><?php echo getGalleryTitle();?></a> | 
+						<?php
+						if (!is_null($_zp_current_album)) {
+							printParentBreadcrumb(); 
+							printAlbumBreadcrumb("", " | "); 
+						} else {
+							$search = new SearchEngine();
+							$params = trim(zp_getCookie('zenphoto_image_search_params'));
+							$search->setSearchParams($params);
+							$images = $search->getImages(0);
+							$searchwords = $search->words;
+							$searchdate = $search->dates;
+							$searchfields = $search->fields;
+							$page = $search->page;
+							$returnpath = getSearchURL($searchwords, $searchdate, $searchfields, $page);
+							echo '<a href='.$returnpath.'><em>'.gettext('Search').'</em></a> | ';
+						}
+						?> </span> 
+						Slideshow
+					</div> <!-- navbar -->
+				</div> <!-- wrapnav -->
+			</div> <!-- galleryheader -->
+		</div> <!-- main4 -->
+		<div id="content">
+ 			<div id="main">
+				<div id="slideshowpage">
+					<?php printSlideShow(false,true); ?>
 				</div>
 			</div>
-		</div>
-	</div>
+		</div> <!-- content -->
+	</div> <!-- main2 -->
 	<!-- Footer -->
 	<div class="footlinks">
 		<?php 
@@ -55,6 +64,7 @@ $themeResult = getTheme($zenCSS, $themeColor, 'effervescence');
 		?>
 		<a href="http://www.zenphoto.org" title="<?php echo gettext('A simpler web photo album'); ?>"><?php echo gettext('Powered by').' ';?>
 		<font face="Arial Narrow" size="4">zen</font><span style="font-variant: small-caps"><font face="Arial Black" size="1">photo</font></span></a>
-	</div>
+	</div> <!-- footlinks -->
+	<?php printAdminToolbox(); ?>
 </body>
 </html>
