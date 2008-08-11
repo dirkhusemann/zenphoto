3738479f5dca2cd81c1afe1eca87c12e8e7040ca
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-08-11 17:45:33 +0000

    Setup will re-run the system check after a login
    git-svn-id: http://www.zenphoto.org/svn/trunk@2160 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/setup.php
+++ zp-core/setup.php
@@ -703,8 +703,9 @@ if (file_exists("zp-config.php")) {
 
 		if (!($tables[$_zp_conf_vars['mysql_prefix'].'administrators'] == 'create')) {
 			if (!($_zp_loggedin & ADMIN_RIGHTS) && (!isset($_GET['create']) && !isset($_GET['update']))) {  // Display the login form and exit.
+				if (!empty($mod)) $mod = '?'.substr($mod, 1);
 				if ($_zp_loggedin) { echo "<br /><br/>".gettext("You need <em>USER ADMIN</em> rights to run setup."); }
-				printLoginForm("/" . ZENFOLDER . "/setup.php?checked$mod", false);
+				printLoginForm("/" . ZENFOLDER . "/setup.php$mod", false);
 				echo "\n</div>";
 				printAdminFooter();
 				echo "\n</body>";
