b0f7c5ca1ebb1df7f4afbd3e72d287eab4ed8038
krainbolt <krainbolt@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-08-28 23:27:36 +0000

    Added title to HEAD in slideshow.
    git-svn-id: http://www.zenphoto.org/svn/trunk@2342 46129fc4-274e-7f48-8cfa-5da4c607681c

--- themes/default/slideshow.php
+++ themes/default/slideshow.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<link rel="stylesheet" href="<?php echo $_zp_themeroot; ?>/slideshow.css" type="text/css" />
-<?php printSlideShowJS(); ?>
-
+	<title><?php echo getBareGalleryTitle(); ?></title>
+	<link rel="stylesheet" href="<?php echo $_zp_themeroot; ?>/slideshow.css" type="text/css" />
+	<?php printSlideShowJS(); ?>
 </head>
+
 <body>
 	<div id="slideshowpage">
 			<?php printSlideShow(true,true); ?>
