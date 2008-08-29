a1dd86406b95e7eedecc5fd5d7226ddd257bed61
krainbolt <krainbolt@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-08-29 22:51:23 +0000

    Fixed problems with thickbox packaged images.
    git-svn-id: http://www.zenphoto.org/svn/trunk@2355 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/js/js-string-constants.js.php
+++ zp-core/js/js-string-constants.js.php
@@ -1,4 +1,6 @@
 <?php
+if (!defined('ZENFOLDER')) { define('ZENFOLDER', 'zp-core'); }
+
 header("Last-Modified: " . gmdate("D, d M Y H:i:s", time()-3600*24*30) . " GMT"); // Date in the past
 header("Expires: " . gmdate("D, d M Y H:i:s", time()+3600*24*60) . " GMT"); // Don't expire for 60 days
 header("Cache-Control: max-age=86400, s-maxage=86400, proxy-revalidate, must-revalidate");
@@ -7,6 +9,8 @@ header("Content-Type: application/x-javascript");
 require_once('../functions.php');
 
 ?>
+
+<?php echo "var zppath = \"/".ZENFOLDER."\";"; ?>
 
 var zpstrings = {
 	'Test' : "<?php echo gettext('Test'); ?>",
@@ -21,6 +25,7 @@ var zpstrings = {
 	'Cancel' : "<?php echo gettext('cancel'); ?>",
 	'CurrentlyEditingSomethingElse' : "<?php echo gettext('Currently editing something else, save or cancel to edit this.'); ?>",
 	'Close' : "<?php echo gettext('Close'); ?>",
+	'close' : "<?php echo gettext('close'); ?>",
 	'orEscKey' : "<?php echo gettext('or Esc Key'); ?>",
 	'Next' : "<?php echo gettext('Next'); ?>",
 	'Prev' : "<?php echo gettext('Prev'); ?>",
