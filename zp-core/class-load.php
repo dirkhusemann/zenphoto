2f8cb6b8634f025fdc93748b9dec3c2a9c1e7fc9
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-09-08 15:58:22 +0000

    Fixes 739--Missing file from SVN
    git-svn-id: http://www.zenphoto.org/svn/trunk@2407 46129fc4-274e-7f48-8cfa-5da4c607681c

---
+++ zp-core/class-load.php
@@ -0,0 +1,14 @@
+<?php
+/*******************************************************************************
+* Load the base classes (Image, Album, Gallery, etc.)                          *
+*******************************************************************************/
+
+require_once('classes.php');
+require_once('class-image.php');
+require_once('class-album.php');
+require_once('class-gallery.php');
+require_once('class-search.php');
+require_once('class-transientimage.php');
+
+
+?>
