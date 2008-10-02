f0f03fb40002719c9cd586f1260cee97b59cb670
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-10-02 00:41:53 +0000

    styling change
    git-svn-id: http://www.zenphoto.org/svn/trunk@2646 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/admin.php
+++ zp-core/admin.php
@@ -768,7 +768,7 @@ if ($allimagecount) {
 
 			<tr class="imageextrainfo" style="display: none;">
 				<td align="right" valign="top"><?php echo gettext("Date:"); ?></td>
-				<td><input type="text" size="48" name="<?php echo $currentimage; ?>-date"
+				<td><input type="text" size="<?php echo TEXT_INPUT_SIZE; ?>" name="<?php echo $currentimage; ?>-date"
 					value="<?php $d=$image->getDateTime(); if ($d!='0000-00-00 00:00:00') { echo $d; } ?>" /></td>
 			</tr>
 
