df13afd09e8da9d45fa92b4eda42e46eb04b6e46
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-11-21 16:04:28 +0000

    Adds missing gettext to user registration form
    git-svn-id: http://www.zenphoto.org/svn/trunk@2942 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/plugins/register_user/register_user_form.php
+++ zp-core/plugins/register_user/register_user_form.php
@@ -2,23 +2,23 @@
 		<input type="hidden" name="register_user" value="yes" />
 		<table class="register_user">
 		<tr>
-			<td>Name:</td>
+			<td><?php echo gettext("Name:"); ?></td>
 			<td><input type="text" id="admin_name" name="admin_name" value="<?php echo $admin_n; ?>" /></td>
 		</tr>
 		<tr>
-			<td>User ID:</td>
+			<td><?php echo gettext("User ID:"); ?></td>
 			<td><input type="text" id="adminuser" name="adminuser" value="<?php echo $user; ?>" /></td>
 		</tr>
 		<tr>
-			<td>Password:</td>
+			<td><?php echo gettext("Password:"); ?></td>
 			<td><input type="password" id="adminpass" name="adminpass"	value="" /></td>
 		</tr>
 		<tr>
-			<td>re-enter:</td>
+			<td><?php echo gettext("re-enter:"); ?></td>
 			<td><input type="password" id="adminpass_2" name="adminpass_2"	value="" /></td>
 		</tr>
 		<tr>
-			<td>Email:</td>
+			<td><?php echo gettext("Email:"); ?></td>
 			<td><input type="text" id="admin_email" name="admin_email" value="<?php echo $admin_e; ?>" /></td>
 		</tr>
 		</table>
