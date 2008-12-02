135e1fa641bd1ea9ab795a50b7f4e465b026374d
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2008-12-02 16:39:42 +0000

    fixes #889
    git-svn-id: http://www.zenphoto.org/svn/trunk@2982 46129fc4-274e-7f48-8cfa-5da4c607681c

--- themes/stopdesign/themeoptions.php
+++ themes/stopdesign/themeoptions.php
@@ -11,10 +11,11 @@ class ThemeOptions {
 	
 	function getOptionsSupported() {
 		return array(	gettext('Allow comments') => array('key' => 'Allow_comments', 'type' => 1, 'desc' => gettext('Check to enable comment section.')),
-									gettext('Allow search') => array('key' => 'Allow_search', 'type' => 5,
-													'selections' => array(gettext('Recent images') => 'Recent images', gettext('Random images') => 'Random images'),
+									gettext('Allow search') => array('key' => 'Allow_search', 'type' => 1,
 													'desc' => gettext('Check to enable search form.')),
-									gettext('Mini slide selector') => array('key' => 'Mini_slide_selector', 'type' => 2, 'desc' => gettext('Select what you want for the six special slides.'))
+									gettext('Mini slide selector') => array('key' => 'Mini_slide_selector', 'type' => 5,
+													'selections' => array(gettext('Recent images') => 'Recent images', gettext('Random images') => 'Random images'),
+													'desc' => gettext('Select what you want for the six special slides.'))
 									);
 	}
 	function handleOption($option, $currentValue) {
