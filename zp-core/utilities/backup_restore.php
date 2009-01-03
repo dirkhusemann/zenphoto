600aba9078e2c864327b4264daea7caa0f4b5ec0
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-01-03 18:13:08 +0000

    fixes #899
    git-svn-id: http://www.zenphoto.org/svn/trunk@3204 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/utilities/backup_restore.php
+++ zp-core/utilities/backup_restore.php
@@ -14,6 +14,7 @@ $button_icon = 'images/folder.png';
 $button_rights = ADMIN_RIGHTS;
 
 define('OFFSET_PATH', 3);
+define('HEADER', '__HEADER__');
 define('RECORD_SEPARATOR', ':****:');
 define('TABLE_SEPARATOR', '::');
 define('RESPOND_COUNTER', 1000);
@@ -46,19 +47,61 @@ function fillbuffer($handle) {
 	return true;
 }
 function getrow($handle) {
-	global $buffer;
-	global $counter;
-	$end = strpos($buffer, RECORD_SEPARATOR);
-	while ($end === false) {
-		if ($end = fillbuffer($handle)) {
-			$end = strpos($buffer, RECORD_SEPARATOR);
-		} else {
-			return false;
+	global $buffer, $counter, $file_version;
+	if ($file_version == 0 || substr($buffer, 0, strlen(HEADER)) == HEADER) {
+		$end = strpos($buffer, RECORD_SEPARATOR);
+		while ($end === false) {
+			if ($end = fillbuffer($handle)) {
+				$end = strpos($buffer, RECORD_SEPARATOR);
+			} else {
+				return false;
+			}
 		}
+		$result = substr($buffer, 0, $end);
+		$buffer = substr($buffer, $end+strlen(RECORD_SEPARATOR));
+	} else {
+		$i = strpos($buffer, ':');
+		if ($i === false) {
+			fillbuffer($handle);
+			$i = strpos($buffer, ':');
+		}
+		$end = substr($buffer, 0, $i)+$i+1;
+		while ($end >= strlen($buffer)) {
+			if (!fillbuffer($handle)) return false;
+		}
+		$result = substr($buffer, $i+1, $end-$i-1);
+		$buffer = substr($buffer, $end);
 	}
-	$result = substr($buffer, 0, $end);
-	$buffer = substr($buffer, $end+strlen(RECORD_SEPARATOR));
 	return $result;
+}
+
+function compress($str, $lvl) {
+	global $compression_handler;
+	switch ($compression_handler) {
+		case 'no':
+			return $str;
+		case 'bzip2':
+			return bzcompress($str, $lvl);
+		default:
+			return gzcompress($str, $lvl);
+	}
+}
+
+function decompress($str) {
+	global $compression_handler;
+	switch ($compression_handler) {
+		case 'no':
+			return $str;
+		case 'bzip2':
+			return bzdecompress($str);
+		default:
+			return gzuncompress($str);
+	}
+}
+
+function writeHeader($type, $value) {
+	global $handle;
+	return fwrite($handle, HEADER.$type.'='.$value.RECORD_SEPARATOR);
 }
 
 $gallery = new Gallery();
@@ -76,6 +119,17 @@ echo '</head>';
 <h1><?php echo (gettext('Backup and Restore your Database')); ?></h1>
 <?php
 if (isset($_REQUEST['backup']) && db_connect()) {
+	$compression_level = sanitize($_REQUEST['compress'],3);
+	setOption('backup_compression', $compression_level);
+	if ($compression_level > 0) {
+		if (function_exists('bzcompress')) {
+			$compression_handler = 'bzip2';
+		} else {
+			$compression_handler = 'gzip';
+		}
+	} else {
+		$compression_handler = 'no';
+	}
 	$prefix = substr(prefix(''), 1, -1);
 	$sql = "SHOW TABLES FROM `".$_zp_conf_vars['mysql_database']."` LIKE '".$prefix."%';";
 	$result = query_full_array($sql);
@@ -90,6 +144,12 @@ if (isset($_REQUEST['backup']) && db_connect()) {
 		if ($handle === false) {
 			printf(gettext('Failed to open %s for writing.'), $filename);
 		} else {
+			$writeresult = writeheader('file_version', 1);
+			$writeresult = $writeresult && writeHeader('compression_handler',$compression_handler);
+			if ($writeresult === false) {
+				echo gettext('failed writing to backup!');
+			}
+			
 			$counter = 0;
 			$writeresult = true;
 			foreach ($result as $row) {
@@ -101,10 +161,11 @@ if (isset($_REQUEST['backup']) && db_connect()) {
 					while ($tablerow = mysql_fetch_assoc($result)) {
 						foreach ($tablerow as $key=>$element) {
 							if (!empty($element)) {
-								$tablerow[$key] = gzcompress($element, 4);
+								$tablerow[$key] = compress($element, $compression_level);
 							}
 						}
-						$storestring = $unprefixed_table.TABLE_SEPARATOR.serialize($tablerow).RECORD_SEPARATOR;
+						$storestring = $unprefixed_table.TABLE_SEPARATOR.serialize($tablerow);
+						$storestring = strlen($storestring).':'.$storestring;
 						$writeresult = fwrite($handle, $storestring);
 						if ($writeresult === false) {
 							echo gettext('failed writing to backup!');
@@ -128,7 +189,15 @@ if (isset($_REQUEST['backup']) && db_connect()) {
 	if ($writeresult) {
 		?>
 		<div class="messagebox" id="fade-message">
-		<h2><?php echo gettext("backup complete"); ?></h2>
+		<h2>
+		<?php 
+		if ($compression_level > 0) {
+			printf(gettext('backup completed using <em>%1$s(%2$s)</em> compression'),$compression_handler, $compression_level);
+		} else {
+			echo gettext('backup completed');
+		}
+		?>
+		</h2>
 		</div>
 		<?php
 	} else {
@@ -141,6 +210,8 @@ if (isset($_REQUEST['backup']) && db_connect()) {
 } else if (isset($_REQUEST['restore']) && db_connect()) {
 	$success = false;
 	if (isset($_REQUEST['backupfile'])) {
+		$file_version = 0;
+		$compression_handler = 'gzip';
 		$folder = SERVERPATH . '/' . BACKUPFOLDER .'/';
 		$filename = $folder . UTF8ToFileSystem(sanitize($_REQUEST['backupfile'], 3)).'.zdb';
 		if (file_exists($filename)) {
@@ -148,11 +219,26 @@ if (isset($_REQUEST['backup']) && db_connect()) {
 			if ($handle !== false) {
 				$success = true;
 				$string = getrow($handle);
+				while (substr($string, 0, strlen(HEADER)) == HEADER) {
+					$string = substr($string, strlen(HEADER));
+					$i = strpos($string, '=');
+					$type = substr($string, 0, $i);
+					$what = substr($string, $i+1);
+					switch ($type) {
+						case 'compression_handler':
+							$compression_handler = $what;
+							break;
+						case 'file_version':
+							$file_version = $what;
+					}
+					$string = getrow($handle);
+				}
 				$counter = 0;
 				while (!empty($string)) {
 					$sep = strpos($string, TABLE_SEPARATOR);
 					$table = substr($string, 0, $sep);
-					$row = unserialize(substr($string, $sep+strlen(TABLE_SEPARATOR)));
+					$row = substr($string, $sep+strlen(TABLE_SEPARATOR));
+					$row = unserialize($row);
 					$items = '';
 					$values = '';
 					$updates = '';
@@ -160,7 +246,7 @@ if (isset($_REQUEST['backup']) && db_connect()) {
 					$special_values = '';
 					foreach($row as $key=>$element) {
 						if (!empty($element)) {
-							$element = gzuncompress($element);
+							$element = decompress($element);
 						}
 						$items .= '`'.$key.'`,';
 						if (is_null($element)) {
@@ -193,7 +279,15 @@ if (isset($_REQUEST['backup']) && db_connect()) {
 	if ($success) {
 		?>
 		<div class="messagebox" id="fade-message">
-		<h2><?php echo gettext("restore complete"); ?></h2>
+		<h2>
+		<?php
+		if ($compression_handler == 'no') {
+			echo(gettext('restore completed'));
+		} else {
+			printf(gettext('restore completed using %s compression'), $compression_handler);
+		}
+		?>
+		</h2>
 		</div>
 		<?php
 	} else {
@@ -205,6 +299,7 @@ if (isset($_REQUEST['backup']) && db_connect()) {
 	}
 }
 if (db_connect()) {
+	$compression_level = getOption('backup_compression');
 	?>
 	<h3><?php gettext("database connected"); ?></h3>
 	<p><?php printf(gettext("Your database is <strong>%s</strong>"),getOption('mysql_database')); ?><br />
@@ -212,12 +307,21 @@ if (db_connect()) {
 	</p>
 	<br />
 	<br />
-	<form name="backup_gallery" action=""><input type="hidden" name="backup"
-		value="true">
+	<form name="backup_gallery" action="">
+	<input type="hidden" name="backup" value="true">
 	<div class="buttons pad_button" id="dbbackup">
 	<button class="tooltip" type="submit" title="<?php echo gettext("Backup the tables in your database."); ?>">
 		<img src="<?php echo $webpath; ?>images/burst1.png" alt="" /> <?php echo gettext("Backup the Database"); ?>
 	</button>
+	<select name="compress">
+	<?php
+	for ($v=0; $v<=9; $v++) {
+	?>
+		<option value="<?php echo $v;?>"<?php if($compression_level == $v) echo ' SELECTED'; ?>><?php echo $v; ?></option>
+	<?php
+	}
+	?>
+	</select> Compression level
 	</div>
 	<br clear="all" />
 	<br clear="all" />
@@ -245,6 +349,8 @@ if (db_connect()) {
 		</form>
 	<?php
 	}
+	?>
+	<?php
 } else {
 	echo "<h3>".gettext("database not connected")."</h3>";
 	echo "<p>".gettext("Check the zp-config.php file to make sure you've got the right username, password, host, and database. If you haven't created the database yet, now would be a good time.");
@@ -252,6 +358,8 @@ if (db_connect()) {
 echo	'<p>';
 echo gettext('The backup facility creates database snapshots in the <code>backup</code> folder of your installation.').' '; 
 echo gettext('These backups are named in according to the date and time the backup was taken.').' ';
+echo gettext('The compression level goes from 0 (no compression) to 9 (maximum compression). Higher compression requires more processing and may not result in much space savings.').' ';
+echo '<br /><br />';
 echo gettext('You restore your database by selecting a backup and pressing the <em>Restore the Database</em> button').' ';
 echo gettext('The restore is “additive”. That is the database is not emptied before the restore is attempted.');
 echo '</p>'
