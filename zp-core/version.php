87de4b9cb88e390375de882896caa7cf1bf9696f
buildscript <buildscript@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-02-01 19:55:04 +0000

    Release script: build tag updated to 3427 and version updated to 1.2.3
    git-svn-id: http://www.zenphoto.org/svn/trunk@3427 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,4 +1,4 @@
 <?php // This file contains version info only and is automatically updated. DO NOT EDIT.
-define('ZENPHOTO_VERSION', '1.2.2');
-define('ZENPHOTO_RELEASE', 3423);
+define('ZENPHOTO_VERSION', '1.2.3');
+define('ZENPHOTO_RELEASE', 3427);
 ?>
