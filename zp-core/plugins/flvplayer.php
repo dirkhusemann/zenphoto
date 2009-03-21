f740d312ddc29af7d390734d884bb2937ed36ac8
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-03-21 16:56:28 +0000

    flv player plugin: The former separate plugin flv_playlist is now incorporated for convenience.
    git-svn-id: http://www.zenphoto.org/svn/trunk@3666 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/plugins/flvplayer.php
+++ zp-core/plugins/flvplayer.php
@@ -1,16 +1,19 @@
 <?php
 /**
  * flvplayer -- plugin support for the flvplayer flash video player. Support for version 3 and 4.
- * NOTE: Flash players do not support external albums!
+ * 
+ * Note: 1.1 now incorporates the former separate flv_playlist plugin to show the content of an media album with .flv/.mp4/.mp3 movie/audio files as a playlist or as separate players with flv player. Note:</strong>Currently internally uses FLV player version 3 because of API incompatiblity with version 4.
+ * 
+ * IMPORTANT: Flash players do not support external albums!
  *
  * @author Malte Müller (acrylian), Stephen Billard (sbillard)
- * @version 1.0.3
+ * @version 1.1
  * @package plugins
  */
 
-$plugin_description = ($external = (getOption('album_folder_class') === 'external'))? gettext('<strong>Flash players do not support <em>External Albums</em>!</strong>'): gettext("Enable <strong>FLV</strong> player to handle multimedia files. IMPORTANT: Only one multimedia player plugin can be enabled at the time.<br> <strong>NOTE: You need to buy a licence from the player's developer LongTail Video if you intend to use this plugin for commercial purposes.</strong> Please see <a href='http://www.longtailvideo.com/players/jw-flv-player/'>LongTail Video - JW players</a> for more info about the player and its licence.");
+$plugin_description = ($external = (getOption('album_folder_class') === 'external'))? gettext('<strong>Flash players do not support <em>External Albums</em>!</strong>'): gettext("Enable <strong>FLV</strong> player to handle multimedia files. IMPORTANT: Only one multimedia player plugin can be enabled at the time.<br />Version 1.1. now incorporates the flv_playlist plugin to show the content of an media album with .flv/.mp4/.mp3 movie/audio files only as a playlist or as separate players with flv player.<strong>Note:</strong>Currently supports only FLV player version 3.<br> <strong>NOTE: You need to buy a licence from the player's developer LongTail Video if you intend to use this plugin for commercial purposes.</strong> Please see <a href='http://www.longtailvideo.com/players/jw-flv-player/'>LongTail Video - JW players</a> for more info about the player and its licence.");
 $plugin_author = "Malte Müller (acrylian), Stephen Billard (sbillard)";
-$plugin_version = '1.0.3';
+$plugin_version = '1.1';
 $plugin_URL = "http://www.zenphoto.org/documentation/plugins/_plugins---flvplayer.php.html";
 $plugin_disable = $external;
 $option_interface = new flvplayer();
@@ -42,6 +45,13 @@ class flvplayer {
 		setOptionDefault('flv_player_version','player3');
 		setOptionDefault('flv_player_controlbar','over');
 		//setOptionDefault('flv_player_ignoresize_for_mp3', 'true');
+		
+		// flv_playlist options
+		setOptionDefault('flvplaylist_width', '600');
+		setOptionDefault('flvplaylist_height', '240');
+		setOptionDefault('flvplaylist_displaywidth', '320');
+		setOptionDefault('flvplaylist_displayheight', '240');
+		setOptionDefault('flvplaylist_thumbsinplaylist', '');
 	}
 
 	function getOptionsSupported() {
@@ -69,6 +79,17 @@ class flvplayer {
 		gettext('Controlbar position') => array('key' => 'flv_player_controlbar', 'type' => 5,
 										'selections' => array(gettext('Bottom')=>"bottom", gettext('Over')=>"over", gettext('None')=>"none"),
 										'desc' => gettext("The position of the controlbar. <em>Player version 4 only!</em>")),
+		gettext('Playlist Width') => array('key' => 'flvplaylist_width', 'type' => 0,
+										'desc' => gettext("Player width for the playlist")),
+		gettext('Playlist Height') => array('key' => 'flvplaylist_height', 'type' => 0,
+										'desc' => gettext("Player height for the playlist (ignored for .<em>mp3</em> files if there is no preview image available.)")),
+		gettext('Playlist Displaywidth') => array('key' => 'flvplaylist_displaywidth', 'type' => 0,
+										'desc' => gettext("Display width for the playlist. The display width is needed for the playlist menu to be shown. In this case the 'displaywidth - width = width of the playlist menu'. See the flv player site for more info about these options.")),
+		gettext('Playlist Displayheight') => array('key' => 'flvplaylist_displayheight', 'type' => 0,
+										'desc' => gettext("Display height for the playlist. If the width is too small to show the playlist menu, you can set the height higher to show it below the actual movie display. See the flv player site for more info about these options.")),
+		gettext('Playlist Thumbs in playlist') => array('key' => 'flvplaylist_thumbsinplaylist', 'type' => 1,
+										'desc' => gettext("Check if you want that thumbnails of the preview images should be shown in the playlist."))
+		
 		
 		
 		);
@@ -184,4 +205,106 @@ class flvplayer {
 		return false;
 	}
 }
+
+/**
+ * To show the content of an media album with .flv/.mp4/.mp3 movie/audio files only as a playlist or as separate players with flv player
+ * NOTE: The flv player plugin needs to be installed (This plugin currently internally uses FLV player 3 because of FLV player 4 Api changes!)
+ * 
+ * The playlist is meant to replace the 'next_image()' loop on a theme's album.php.
+ * It can be used with a special 'album theme' that can be assigned to media albums with with .flv/.mp4/.mp3s 
+
+ * movie/audio files only. See the examples below 
+ * You can either show a 'one player window' playlist or show all items as separate players paginated 
+
+ * (set in the settings for thumbs per page) on one page (like on a audio or podcast blog).
+ *
+ * If there is no preview image for a mp3 file existing only the player control bar is shown.
+ * 
+ * The two modes:
+ * a) 'playlist'
+ * Replace the entire 'next_image()' loop on album.php with this:
+ * <?php flvPlaylist("playlist"); ?>
+ * 
+ * b) 'players'
+ * Modify the 'next_image()' loop on album.php like this:
+ * <?php	
+ * while (next_image(false,$firstPageImages)):
+ * printImageTitle();
+ * flvPlaylist("players");
+ * endwhile;
+ * ?>
+ * Of course you can add further functions to b) like title, description, date etc., too.
+ *  
+ */
+function flvPlaylist($option='') {
+	global $_zp_current_album, $_zp_current_image;
+	if(checkAlbumPassword($_zp_current_album->getFolder(), $hint)) {
+		if($option === "players") {
+			$moviepath = getUnprotectedImageURL();
+			$ext = strtolower(strrchr(getUnprotectedImageURL(), "."));
+		}
+		$imagetitle = getImageTitle();
+	}
+	$albumid = getAlbumID();
+
+	switch($option) {
+		case "playlist":
+			?>
+	<div id="flvplaylist"><?php echo gettext("The flv player is not installed. Please install or activate the flv player plugin."); ?></div>
+	<script type="text/javascript">
+		var so = new SWFObject('<?php echo WEBPATH."/".ZENFOLDER.PLUGIN_FOLDER; ?>flvplayer/player3.swf','jstest','<?php echo getOption('flvplaylist_width'); ?>','<?php echo getOption('flvplaylist_height'); ?>','8');
+		so.addParam('allowfullscreen','true');
+		so.addParam('overstretch','true');
+		so.addVariable('displaywidth', '<?php echo getOption('flvplaylist_displaywidth'); ?>');
+		so.addVariable('displayheight','<?php echo getOption('flvplaylist_displayheight'); ?>');
+		so.addVariable('backcolor','<?php echo getOption('flv_player_backcolor'); ?>');
+		so.addVariable('frontcolor','<?php echo getOption('flv_player_frontcolor'); ?>');
+		so.addVariable('lightcolor','<?php echo getOption('flv_player_lightcolor'); ?>');
+		so.addVariable('screencolor','<?php echo getOption('flv_player_screencolor'); ?>');
+		so.addVariable('file','<?php echo WEBPATH."/".ZENFOLDER.PLUGIN_FOLDER; ?>flvplayer/playlist.php?albumid=<?php echo $albumid; ?>');
+		so.addVariable('javascriptid','jstest');
+		so.addVariable('enablejs','true');
+		so.addVariable('thumbsinplaylist','<?php echo (getOption('flvplaylist_thumbsinplaylist') ? 'true' : 'false') ?>');
+		so.write('flvplaylist');
+	</script>
+	<?php
+		break;
+
+		case "players":
+			if (($ext == ".flv") || ($ext == ".mp3") || ($ext == ".mp4")) {
+				echo "<div id=\"flvplaylist-".$imagetitle."\">".gettext("The flv player is not installed. Please install or activate the flv player plugin..")."</div>";
+					
+				// check if an image/videothumb is available - this shouldn't be hardcoded...
+				$album = $_zp_current_image->getAlbum();
+				$videoThumb = checkObjectsThumb($album->localpath, $_zp_current_image->filename);
+				if (!empty($videoThumb)) {
+					$videoThumb = getAlbumFolder(WEBPATH).$album->name.'/'.$videoThumb;
+				}										
+		?>
+		<script type="text/javascript">
+		 <?php 
+
+		 if(($ext == ".mp3") && empty($videoThumb)) { 
+		 ?> 
+			var so = new SWFObject('<?php echo WEBPATH."/".ZENFOLDER.PLUGIN_FOLDER; ?>flvplayer/player3.swf','jstest','<?php echo getOption('flvplaylist_width'); ?>','20','8');
+		<?php } else { ?>
+			var so = new SWFObject('<?php echo WEBPATH."/".ZENFOLDER.PLUGIN_FOLDER; ?>flvplayer/player3.swf','jstest','<?php echo getOption('flvplaylist_width'); ?>','<?php echo getOption('flvplaylist_height'); ?>','8');
+		<?php } ?>
+			so.addParam('allowfullscreen','true');
+			so.addParam('overstretch','true');
+			so.addVariable("image",'<?php echo $videoThumb; ?>');
+			so.addVariable('backcolor','<?php echo getOption('flv_player_backcolor'); ?>');
+			so.addVariable('frontcolor','<?php echo getOption('flv_player_frontcolor'); ?>');
+			so.addVariable('lightcolor','<?php echo getOption('flv_player_lightcolor'); ?>');
+			so.addVariable('screencolor','<?php echo getOption('flv_player_screencolor'); ?>');
+			so.addVariable('file','<?php echo $moviepath; ?>');
+			so.addVariable('javascriptid','jstest');
+			so.addVariable('enablejs','true');
+			so.write('flvplaylist-<?php echo escape($imagetitle); ?>');
+	<?php	} ?>
+		</script>
+	<?php
+	break;
+	}
+} // password check end
 ?>
