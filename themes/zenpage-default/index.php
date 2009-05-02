959b025f99afa80084a05bdf2d069e5df9d5e557
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-05-02 15:34:06 +0000

    Fix for zenpage default theme to use it without Zenpage being enabled
    git-svn-id: http://www.zenphoto.org/svn/trunk@3864 46129fc4-274e-7f48-8cfa-5da4c607681c

--- themes/zenpage-default/index.php
+++ themes/zenpage-default/index.php
@@ -1,8 +1,12 @@
 <?php
-if (checkForPage(getOption("zenpage_homepage"))) { // switch to a news page
-	$ishomepage = true;
-	include ('pages.php');
-} else {
+if(function_exists("checkForPage")) { // check if Zenpage is enabled or not
+	if (checkForPage(getOption("zenpage_homepage"))) { // switch to a news page
+		$ishomepage = true;
+		include ('pages.php');
+	} else {
+		include ('gallery.php');
+	}
+} else { 
 	include ('gallery.php');
 }
 ?>
