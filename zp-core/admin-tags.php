4ad25815494dfa0390c1235a29803db8ca7d4f9f
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-05-21 00:09:59 +0000

    style the tag tab list boxes the same as other admin list boxes
    git-svn-id: http://www.zenphoto.org/svn/trunk@3947 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/admin-tags.php
+++ zp-core/admin-tags.php
@@ -28,160 +28,201 @@ if (isset($_REQUEST['tagsort'])) {
 }
 
 printAdminHeader();
-echo "\n</head>";
-echo "\n<body>";
+?>
+</head>
+<body>
+<?php
 printLogoAndLinks();
-echo "\n" . '<div id="main">';
-printTabs('tags');
-echo "\n" . '<div id="content">';
-
-if (count($_POST) > 0) {
-	if (isset($_GET['newtags'])) {
-		foreach ($_POST as $value) {
-			if (!empty($value)) {
-				$value = mysql_real_escape_string(sanitize($value, 3));
-				$result = query_single_row('SELECT `id` FROM '.prefix('tags').' WHERE `name`="'.$value.'"');
-				if (!is_array($result)) { // it really is a new tag
-					query('INSERT INTO '.prefix('tags').' (`name`) VALUES ("' . $value . '")');
+?>
+<div id="main">
+	<?php
+	printTabs('tags');
+	?>
+	<div id="content">
+		<?php
+		if (count($_POST) > 0) {
+			if (isset($_GET['newtags'])) {
+				foreach ($_POST as $value) {
+					if (!empty($value)) {
+						$value = mysql_real_escape_string(sanitize($value, 3));
+						$result = query_single_row('SELECT `id` FROM '.prefix('tags').' WHERE `name`="'.$value.'"');
+						if (!is_array($result)) { // it really is a new tag
+							query('INSERT INTO '.prefix('tags').' (`name`) VALUES ("' . $value . '")');
+						}
+					}
 				}
-			}
-		}
-	} // newtags
-	if (isset($_GET['delete'])) {
-		$kill = array();
-		foreach ($_POST as $key => $value) {
-			$key = postIndexDecode($key);
-			$kill[] = $_zp_UTF8->strtolower($key);
-		}
-		if (count($kill) > 0) {
-			$sql = "SELECT `id` FROM ".prefix('tags')." WHERE ";
-			foreach ($kill as $tag) {
-				$sql .= "`name`='".(mysql_real_escape_string($tag))."' OR ";
-			}
-			$sql = substr($sql, 0, strlen($sql)-4);
-			$dbtags = query_full_array($sql);
-			if (is_array($dbtags)) {
-				$sqltags = "DELETE FROM ".prefix('tags')." WHERE ";
-				$sqlobjects = "DELETE FROM ".prefix('obj_to_tag')." WHERE ";
-				foreach ($dbtags as $tag) {
-					$sqltags .= "`id`='".$tag['id']."' OR ";
-					$sqlobjects .= "`tagid`='".$tag['id']."' OR ";
+			} // newtags
+			if (isset($_GET['delete'])) {
+				$kill = array();
+				foreach ($_POST as $key => $value) {
+					$key = postIndexDecode($key);
+					$kill[] = $_zp_UTF8->strtolower($key);
 				}
-				$sqltags = substr($sqltags, 0, strlen($sqltags)-4);
-				query($sqltags);
-				$sqlobjects = substr($sqlobjects, 0, strlen($sqlobjects)-4);
-				query($sqlobjects);
-			}
-		}
-	} // delete
-	if (isset($_GET['rename'])) {
-		foreach($_POST as $key=>$newName) {
-			if (!empty($newName)) {
-				$newName = sanitize($newName, 3);
-				$key = postIndexDecode($key);
-				$key = substr($key, 2); // strip off the 'R_'
-				$newtag = query_single_row('SELECT `id` FROM '.prefix('tags').' WHERE `name`="'.mysql_real_escape_string($newName).'"');
-				$oldtag = query_single_row('SELECT `id` FROM '.prefix('tags').' WHERE `name`="'.escape($key).'"');
-				if (is_array($newtag)) { // there is an existing tag of the same name
-					$existing = $newtag['id'] != $oldtag['id']; // but maybe it is actually the original in a different case.
-				} else {
-					$existing = false;
+				if (count($kill) > 0) {
+					$sql = "SELECT `id` FROM ".prefix('tags')." WHERE ";
+					foreach ($kill as $tag) {
+						$sql .= "`name`='".(mysql_real_escape_string($tag))."' OR ";
+					}
+					$sql = substr($sql, 0, strlen($sql)-4);
+					$dbtags = query_full_array($sql);
+					if (is_array($dbtags)) {
+						$sqltags = "DELETE FROM ".prefix('tags')." WHERE ";
+						$sqlobjects = "DELETE FROM ".prefix('obj_to_tag')." WHERE ";
+						foreach ($dbtags as $tag) {
+							$sqltags .= "`id`='".$tag['id']."' OR ";
+							$sqlobjects .= "`tagid`='".$tag['id']."' OR ";
+						}
+						$sqltags = substr($sqltags, 0, strlen($sqltags)-4);
+						query($sqltags);
+						$sqlobjects = substr($sqlobjects, 0, strlen($sqlobjects)-4);
+						query($sqlobjects);
+					}
 				}
-				if ($existing) {
-					query('DELETE FROM '.prefix('tags').' WHERE `id`='.$oldtag['id']);
-					query('UPDATE '.prefix('obj_to_tag').' SET `tagid`='.$newtag['id'].' WHERE `tagid`='.$oldtag['id']);
-				} else {
-					query('UPDATE '.prefix('tags').' SET `name`="'.escape($newName).'" WHERE `id`='.$oldtag['id']);
+			} // delete
+			if (isset($_GET['rename'])) {
+				foreach($_POST as $key=>$newName) {
+					if (!empty($newName)) {
+						$newName = sanitize($newName, 3);
+						$key = postIndexDecode($key);
+						$key = substr($key, 2); // strip off the 'R_'
+						$newtag = query_single_row('SELECT `id` FROM '.prefix('tags').' WHERE `name`="'.mysql_real_escape_string($newName).'"');
+						$oldtag = query_single_row('SELECT `id` FROM '.prefix('tags').' WHERE `name`="'.escape($key).'"');
+						if (is_array($newtag)) { // there is an existing tag of the same name
+							$existing = $newtag['id'] != $oldtag['id']; // but maybe it is actually the original in a different case.
+						} else {
+							$existing = false;
+						}
+						if ($existing) {
+							query('DELETE FROM '.prefix('tags').' WHERE `id`='.$oldtag['id']);
+							query('UPDATE '.prefix('obj_to_tag').' SET `tagid`='.$newtag['id'].' WHERE `tagid`='.$oldtag['id']);
+						} else {
+							query('UPDATE '.prefix('tags').' SET `name`="'.escape($newName).'" WHERE `id`='.$oldtag['id']);
+						}
+					}
 				}
-			}
+			} // rename
 		}
-	} // rename
-}
+		
+		echo "<h1>".gettext("Tag Management")."</h1>";
+		if ($tagsort == 1) {
+			?>
+			<p class="buttons">
+				<a class="tagsort" href="?tagsort=0" title="<?php echo gettext('Sort the tags alphabetically'); ?>">
+					<img src="images/sortorder.png" alt="" /> <?php echo gettext('Order alphabetically'); ?>
+				</a>
+			</p>
+			<br />
+			<br />
+			<br clear: all />
+			<?php
+		} else{
+			?>
+			<p class="buttons">
+				<a class="tagsort" href="?tagsort=1" title="<?php echo gettext('Sort the tags by most used'); ?>">
+					<img src="images/sortorder.png" alt="" /> <?php echo gettext('Order by most used'); ?>
+				</a>
+			</p>
+			<br />
+			<br />
+			<br clear: all />
+			<?php
+		}
+		?>
+		<table class=\"bordered\">
+			<td valign='top'>
+				<h2 class="h2_bordered_edit"><?php echo gettext("Delete tags from the gallery"); ?></h2>
+				<form name="tag_delete" action="?delete=true&amp;tagsort='.$tagsort.'" method="post">
+					<div class="box-tags-unpadded">
+						<?php
+						tagSelector(NULL, '', true, $tagsort);
+						?>
+					</div>
+					<p class="buttons">
+						<button type="submit" id='delete_tags' value="<?php echo gettext("Delete checked tags"); ?>" title="<?php echo gettext("Delete all the tags checked above."); ?>" />
+						<img src="images/fail.png" alt="" /><?php echo gettext("Delete checked tags"); ?>
+						</button>
+					</p>
+					<br clear: all />
+					<br />
+					<br />
+				
+				</form>
+				<div class="tagtext">
+					<p><?php echo gettext('To delete tags from the gallery, place a checkmark in the box for each tag you wish to delete then press the <em>Delete checked tags</em> button. The brackets contain the number of times the tag appears.'); ?></p>
+				</div>
+			</td>
+			
+			<td valign='top'>
+				<h2 class="h2_bordered_edit"><?php echo gettext("Rename tags"); ?></h2>
+				<form name="tag_rename" action="?rename=true&amp;tagsort='.$tagsort.'" method="post">
+					<div class="box-tags-unpadded">
+						<ul class="tagrenamelist">
+							<?php
+							$list = $_zp_admin_ordered_taglist;
+							foreach($list as $item) {
+								$listitem = 'R_'.postIndexEncode($item);
+								?>
+								<li>
+									<label for="<?php echo $listitem; ?>"><?php echo $item; ?></label>
+									<br />
+									<input id="<?php echo $listitem; ?>" name="<?php echo $listitem; ?>" type="text" size='33' />
+								</li>
+								<?php
+							}
+							?>
+						</ul>
+					</div>
+					<p class="buttons">
+					<button type="submit" id='rename_tags' value="<?php echo gettext("Rename tags"); ?>" title="<?php echo gettext("Save all the changes entered above."); ?>" /><img src="images/pass.png" alt="" /><?php echo gettext("Rename tags"); ?></button>
+					</p>
+					<br clear: all />
+					<br />
+					<br />
+				</form>
+				<div class="tagtext">
+					<p><?php echo gettext('To change the value of a tag enter a new value in the text box below the tag. Then press the <em>Rename tags</em> button'); ?></p>
+				</div>
+			</td>
+			
+			<td valign='top'>
+				<h2 class="h2_bordered_edit"><?php echo gettext("New tags"); ?></h2>
+				<form name="new_tags" action="?newtags=true&amp;tagsort='.$tagsort.'"method="post">
+					<div class="box-tags-unpadded">
+						<ul class="tagnewlist">
+							<?php
+							for ($i=0; $i<40; $i++) {
+								?>
+								<li>
+									<input id="new_tag_<?php echo $i; ?>" name="new_tag_<?php echo $i; ?>" type="text" size='33'/>
+								</li>
+								<?php
+							}
+							?>
+						</ul>
+					</div>
+					<p class="buttons">
+					<button type="submit" id='save_tags' value="<?php echo gettext("Add tags"); ?>" title="<?php echo gettext("Add all the tags entered above."); ?>" /><img src="images/add.png" alt="" /><?php echo gettext("Add tags"); ?></button>
+					</p>
+					<br clear: all />
+					<br />
+					<br />
+				
+				</form>
+				<div class="tagtext">				
+					<p><?php echo gettext("Add tags to the list by entering their names in the input fields of the <em>New tags</em> list. Then press the <em>Add tags</em> button"); ?></p>
+				</div>
+			</td>
+			</tr>
+		</table>
+	
+	</div>
+	<?php
+	printAdminFooter();
+	?>
+</div>
+</body>
+</html>
 
-echo "<h1>".gettext("Tag Management")."</h1>";
-if ($tagsort == 1) {
-?>
-<p class="buttons">
-<a class="tagsort" href="?tagsort=0" title="<?php echo gettext('Sort the tags alphabetically'); ?>"><img src="images/sortorder.png" alt="" /> <?php echo gettext('Order alphabetically'); ?></a>
-</p><br /><br /><br clear: all />
-<?php
-} else{
-?>
-<p class="buttons">
-<a class="tagsort" href="?tagsort=1" title="<?php echo gettext('Sort the tags by most used'); ?>"><img src="images/sortorder.png" alt="" /> <?php echo gettext('Order by most used'); ?></a>
-</p><br /><br /><br clear: all />
-<?php
-}
-echo "\n<table class=\"bordered\">";
-echo "\n<tr>";
-echo "\n<th>".gettext("Delete tags from the gallery")."</th>";
-echo "\n<th>".gettext("Rename tags")."</th>";
-echo "\n<th>";
-echo gettext("New tags");
-echo "</th>";
-echo "\n</tr>";
-echo "\n<tr>";
-
-echo "\n<td valign='top'>";
-echo "\n".'<form name="tag_delete" action="?delete=true&amp;tagsort='.$tagsort.'" method="post">';
-tagSelector(NULL, '', true, $tagsort);
-echo "\n<p align='center'>";
-?>
-<p class="buttons">
-<button type="submit" id='delete_tags' value="<?php echo gettext("Delete checked tags"); ?>" title="<?php echo gettext("Delete all the tags checked above."); ?>" /><img src="images/fail.png" alt="" /><?php echo gettext("Delete checked tags"); ?></button>
-</p><br clear: all /><br /><br />
-<?php
-echo "\n</form>";
-echo '<p>'.gettext('To delete tags from the gallery, place a checkmark in the box for each tag you wish to delete then press the <em>Delete checked tags</em> button. The brackets contain the number of times the tag appears.').'</p>';
-echo "\n</td>";
-
-echo "\n<td valign='top'>";
-echo "\n".'<form name="tag_rename" action="?rename=true&amp;tagsort='.$tagsort.'" method="post">';
-echo "\n<ul class=\"tagrenamelist\">";
-$list = $_zp_admin_ordered_taglist;
-foreach($list as $item) {
-	$listitem = 'R_'.postIndexEncode($item);
-	echo "\n".'<li><label for="'.$listitem.'">'.$item.'<br /><input id="'.$listitem.'" name="'.$listitem.'" type="text"';
-	echo " size='33'/></label></li>";
-}
-echo "\n</ul>";
-echo "\n<p align='center'>";
-?>
-<p class="buttons">
-<button type="submit" id='rename_tags' value="<?php echo gettext("Rename tags"); ?>" title="<?php echo gettext("Save all the changes entered above."); ?>" /><img src="images/pass.png" alt="" /><?php echo gettext("Rename tags"); ?></button>
-</p><br clear: all /><br /><br />
-<?php
-
-echo "\n</form>";
-echo '<p>'.gettext('To change the value of a tag enter a new value in the text box below the tag. Then press the <em>Rename tags</em> button').'</p>';
-echo "\n</td>";
-
-echo "\n<td valign='top'>";
-echo '<form name="new_tags" action="?newtags=true&amp;tagsort='.$tagsort.'"method="post">';
-echo "\n<ul class=\"tagnewlist\">";
-for ($i=0; $i<40; $i++) {
-	echo "\n".'<li><label for="new_tag_'.$i.'"><input id="new_tag_'.$i.'" name="new_tag_'.$i.'" type="text"';
-	echo " size='33'/></label></li>";
-}
-echo "\n</ul>";
-echo "\n<p align='center'>";
-?>
-<p class="buttons">
-<button type="submit" id='save_tags' value="<?php echo gettext("Add tags"); ?>" title="<?php echo gettext("Add all the tags entered above."); ?>" /><img src="images/add.png" alt="" /><?php echo gettext("Add tags"); ?></button>
-</p><br clear: all /><br /><br />
-<?php
-echo "\n</form>";
-echo "\n<p>".gettext("Add tags to the list by entering their names in the input fields of the <em>New tags</em> list. Then press the <em>Add tags</em> button").'</p>';
-echo "\n</td>";
-echo "\n</tr>";
-echo "\n</table>";
-
-echo "\n" . '</div>';
-printAdminFooter();
-echo "\n" . '</div>';
-echo "\n</body>";
-echo "\n</html>";
-?>
 
 
 
