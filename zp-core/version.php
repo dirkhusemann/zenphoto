38169d9372433c85e4ebfc4053ef655064ab0746
buildscript <buildscript@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-05-28 06:00:30 +0000

    Nightly script: build tag updated to 3991
    git-svn-id: http://www.zenphoto.org/svn/trunk@3991 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/version.php
+++ zp-core/version.php
@@ -1,4 +1,4 @@
 <?php // This file contains version info only and is automatically updated. DO NOT EDIT.
 define('ZENPHOTO_VERSION', '1.2.5_RC2');
-define('ZENPHOTO_RELEASE', 3988);
+define('ZENPHOTO_RELEASE', 3991);
 ?>
