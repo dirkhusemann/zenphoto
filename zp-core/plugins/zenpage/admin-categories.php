b17b53c19ac161a764ae53e8b75e837074813d6a
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-06-04 12:36:52 +0000

    Zenpage category editing: Now the catlink form is disabled if the checkbox for editing is not checked. Also raises the version number of Zenpage that has been forgotton for Zenphoto 1.2.5.
    git-svn-id: http://www.zenphoto.org/svn/trunk@4034 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/plugins/zenpage/admin-categories.php
+++ zp-core/plugins/zenpage/admin-categories.php
@@ -13,6 +13,22 @@ include("zp-functions.php"); ?>
 <title><?php echo gettext("zenphoto administration"); ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <?php zenpageJSCSS(); ?>  
+<script type="text/javascript">
+<?php if(isset($_GET["edit"])) { // prevent showing the message when adding page or article ?>
+$(document).ready(function() {
+	if(jQuery('#edittitlelink:checked').val() != 1) {
+		$('#catlink').attr("disabled", true); 
+	}
+	$('#edittitlelink').change(function() {
+		if(jQuery('#edittitlelink:checked').val() == 1) {	
+			$('#catlink').removeAttr("disabled"); 
+		} else {
+			$('#catlink').attr("disabled", true); 
+		}
+	});
+});
+<?php } ?>
+</script>
 </head>
 <body>
 <?php 
