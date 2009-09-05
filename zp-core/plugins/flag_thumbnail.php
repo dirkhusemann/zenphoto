3d091ae6c612b7e1635a22aa1e89f80b283fdd54
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-09-05 19:52:55 +0000

    plugin to overlay lock image over password protected album thumbs and not published icon over not published album/image thumbnails
    git-svn-id: http://www.zenphoto.org/svn/trunk@4435 46129fc4-274e-7f48-8cfa-5da4c607681c

---
+++ zp-core/plugins/flag_thumbnail.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * 
+ * Apply a lock image over thumbnails of password protected albums
+ * 
+ * To use simply enable the plugin.
+ * 
+ */
+$plugin_description = sprintf(gettext('Apply <img src="%1$s/images/lock.png"/> over thumbnails of <em>password protected</em> albums and <img src="%1$s/images/action.png"/> over thumbnails of <em>not published</em> albums and <em>not visible</em> images.'),WEBPATH.'/'.ZENFOLDER);
+$plugin_author = "Stephen Billard (sbillard)";
+$plugin_version = '1.2.7';
+$plugin_URL = "http://www.zenphoto.org/documentation/plugins/_plugins---flag_thumbnail.php.html";
+
+zp_register_filter('standard_image_thumb_html', 'flag_thumbnail_std_image_thumbs');
+zp_register_filter('standard_album_thumb_html', 'flag_thumbnail_std_album_thumbs', 1);
+zp_register_filter('custom_album_thumb_html', 'flag_thumbnail_custom_album_thumbs', 1);
+zp_register_filter('custom_image_html', 'flag_thumbnail_custom_images', 1);
+
+function flag_thumbnail_insert_class($html) {
+	$i = strpos($html, 'class=');
+	if ($i !== false) {
+		$img = '';
+		if (strpos($html, 'password_protected', $i+7) !== false) {
+			$img = WEBPATH.'/'.ZENFOLDER.'/images/lock.png';
+		} else if (strpos($html, 'not_visible', $i+7) !== false) {
+			$img = WEBPATH.'/'.ZENFOLDER.'/images/action.png';
+		}
+		if ($img) {
+			$html = '<div style="position:relative; display:block;">'."\n".
+							$html."\n".
+							'<img src="'.$img.'" style="position: absolute;top: 4px;left: 4px;"/>'."\n".'</div>'."\n";
+		}
+	}
+	return $html;
+}
+
+function flag_thumbnail_custom_images($html, $thumbstandin) {
+	if ($thumbstandin) {
+		$html = flag_thumbnail_insert_class($html);
+	}
+	return $html;
+}
+function flag_thumbnail_std_image_thumbs($html) {
+	$html = flag_thumbnail_insert_class($html);
+	return $html;
+}
+function flag_thumbnail_std_album_thumbs($html) {
+	$html = flag_thumbnail_insert_class($html);
+	return $html;
+}
+function flag_thumbnail_custom_album_thumbs($html) {
+	$html = flag_thumbnail_insert_class($html);
+	return $html;
+}
+
+?>
