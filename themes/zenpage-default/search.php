30c12b0776ecd35dfed9d362f553fd9699ec4e8a
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-12-06 20:17:31 +0000

    Zenpage-default theme basic search page update for pages and articles
    git-svn-id: http://www.zenphoto.org/svn/trunk@4764 46129fc4-274e-7f48-8cfa-5da4c607681c

--- themes/zenpage-default/search.php
+++ themes/zenpage-default/search.php
@@ -30,31 +30,91 @@ header('Last-Modified: ' . gmdate('D, d M Y H:i:s').' GMT');
 
 		<div id="content">
 		<div id="content-left">
+				<h2>
 		<?php
-		if (($total = getNumImages() + getNumAlbums()) > 0) {
-			if (isset($_REQUEST['date'])){
-				$searchwords = getSearchDate();
- 		} else { $searchwords = getSearchWords(); }
-			echo "<p>".gettext("Total matches for")." <em>".$searchwords."</em>: $total</p>";
-		}
+			$numimages = getNumImages();
+			$numalbums = getNumAlbums();
+			$total = $numimages + $numalbums;
+			if ($zenpage = getOption('zp_plugin_zenpage')) {
+				$numpages = getNumPages();
+				$numnews = getNumNews();
+				$total = $total + $numnews + $numpages;
+			} else {
+				$numpages = $numnews = 0;
+			}
+			$searchwords = getSearchWords();
+			$searchdate = getSearchDate();
+			if (!empty($searchdate)) {
+				if (!empty($seachwords)) {
+					$searchwords .= ": ";
+				}
+				$searchwords .= $searchdate;
+			}
+			if ($total > 0 ) {
+				printf(ngettext('%1$u Hit for <em>%2$s</em>','%1$u Hits for <em>%2$s</em>',$total), $total, $searchwords); 
+			}
 		$c = 0;
 		?>
-<div id="albums">
-			<?php while (next_album()): $c++;?>
-				<div class="album">
-					<div class="thumb">
-						<a href="<?php echo htmlspecialchars(getAlbumLinkURL());?>" title="<?php echo gettext('View album:');?> <?php echo getBareAlbumTitle();?>"><?php printAlbumThumbImage(getBareAlbumTitle()); ?></a>
-					</div>
-					<div class="albumdesc">
-						<h3><a href="<?php echo htmlspecialchars(getAlbumLinkURL());?>" title="<?php echo gettext('View album:');?> <?php echo getBareAlbumTitle();?>"><?php printAlbumTitle(); ?></a></h3>
-						<p><?php printAlbumDesc(); ?></p>
-						<small><?php printAlbumDate(gettext("Date:").' '); ?> </small>
-					</div>
-					<p style="clear: both; "></p>
+		
+		<?php if ($_zp_page == 1) { //test of zenpage searches
+			if ($numpages > 0) {
+				?>
+				</h2>
+				<h3><?php printf(gettext('Pages (%s)'),$numpages); ?></h3>
+					<ul>
+					<?php
+					while (next_page()) {
+						?>
+						<li>
+						<h4><?php printPageTitleLink(); ?></h4>
+						<p><?php echo shortenContent(strip_tags($_zp_current_zenpage_page->getContent()),80,getOption("zenpage_textshorten_indicator")); ?></p>
+						</li>
+						<?php
+					}
+					?>
+					</ul>
+				<?php
+				}
+			if ($numnews>0) {
+				?>
+				<h3><?php printf(gettext('Articles (%s)'),$numnews); ?></h3>
+				<div class="zenpagesearchtext">
+					<ul>
+					<?php
+					while (next_news()) {
+						?>
+						<li>
+						<h4><?php printNewsTitleLink(); ?></h4>
+							<p><?php echo shortenContent(strip_tags(getNewsContent()),80,getOption("zenpage_textshorten_indicator")); ?></p>
+						</li>
+						<?php
+					}
+					?>
+					</ul>
 				</div>
-			<?php endwhile; ?>
-			</div>
-
+				<?php
+				}
+			}
+			?>
+		<?php	if (!getOption('search_no_albums') && getNumAlbums() > 0) {	?>
+			<h3><?php printf(gettext('Albums (%s)'),$numalbums); ?></h3>
+			<div id="albums">
+				<?php while (next_album()): ?>
+					<div class="album">
+							<div class="thumb">
+							<a href="<?php echo htmlspecialchars(getAlbumLinkURL());?>" title="<?php echo gettext('View album:'); ?> <?php echo getBareAlbumTitle();?>"><?php printCustomAlbumThumbImage(getBareAlbumTitle(), NULL, 95, 95, 95, 95); ?></a>
+ 							 </div>
+								<div class="albumdesc">
+									<h3><a href="<?php echo htmlspecialchars(getAlbumLinkURL());?>" title="<?php echo gettext('View album:'); ?> <?php echo getBareAlbumTitle();?>"><?php printAlbumTitle(); ?></a></h3>
+ 									<?php printAlbumDate(""); ?>
+									<p><?php echo truncate_string(getAlbumDesc(), 45); ?></p>
+								</div>
+					</div>
+				<?php endwhile; ?>
+		</div>
+	<?php } ?>
+<?php if (getNumImages() > 0) { ?>
+<h3><?php printf(gettext('Images (%s)'),$numimages); ?></h3>
 			<div id="images">
 				<?php while (next_image(false, $firstPageImages)): $c++;?>
 				<div class="image">
@@ -63,10 +123,11 @@ header('Last-Modified: ' . gmdate('D, d M Y H:i:s').' GMT');
 				<?php endwhile; ?>
 			</div>
 		<br clear=all>
+<?php } ?>
 		<?php
 		if (function_exists('printSlideShowLink')) printSlideShowLink(gettext('View Slideshow'));
-		if ($c == 0) {
-				echo "<p>".gettext("Sorry, no image matches. Try refining your search.")."</p>";
+		if ($total == 0) {
+				echo "<p>".gettext("Sorry, no matches. Try refining your search.")."</p>";
 			}
 
 			printPageListWithNav("&laquo; ".gettext("prev"),gettext("next")." &raquo;");
