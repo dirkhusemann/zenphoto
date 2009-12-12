99f5bc0d779008ff52cdb7d1a3b39f4f60f77716
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2009-12-12 23:11:17 +0000

    use default search fields if parameter not supplied
    git-svn-id: http://www.zenphoto.org/svn/trunk@4801 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/zp-extensions/captcha/zenphoto.php
+++ zp-core/zp-extensions/captcha/zenphoto.php
@@ -42,7 +42,7 @@ class captcha {
 												'desc' => gettext('The font to use for captcha characters.')),
 								'' 			=> array('key' => 'zenphoto_captcha_image', 'type' => OPTION_TYPE_CUSTOM,
 												'order' => 4,
-												'desc' => gettext('Simple cpatcha'))
+												'desc' => gettext('Sample Cpatcha image'))
 								);
 	}
 	function handleOption($key, $cv) {
