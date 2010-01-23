55a8501bc0c2b85795042b892dade876edeb0b92
sbillard <sbillard@46129fc4-274e-7f48-8cfa-5da4c607681c>
2010-01-23 02:26:25 +0000

    only load the news rss lib if we are going to use it
    git-svn-id: http://www.zenphoto.org/svn/trunk@4964 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/zp-extensions/zenphoto_news.php
+++ zp-core/zp-extensions/zenphoto_news.php
@@ -8,11 +8,11 @@ $plugin_disable = $disable;
 
 if (!$plugin_disable) {
 	zp_register_filter('admin_overview_left', 'printNews');
-	require_once(dirname(__FILE__).'/zenphoto_news/rsslib.php'); 
 }
 
 function printNews($discard) {
-?>
+	require_once(dirname(__FILE__).'/zenphoto_news/rsslib.php'); 
+	?>
 	<div class="box" id="overview-news">
 		<h2 class="h2_bordered"><?php echo gettext("News from Zenphoto.org"); ?></h2>
 		<?php echo RSS_Display("http://www.zenphoto.org/category/News/feed", 3); ?>
