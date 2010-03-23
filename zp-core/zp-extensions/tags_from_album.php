21965cc908d96b5b307e2b2ba4fe843225b54e82
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2010-03-23 19:06:45 +0000

    Improvements for tags_from_album plugin
    git-svn-id: http://www.zenphoto.org/svn/trunk@5154 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/zp-extensions/tags_from_album.php
+++ zp-core/zp-extensions/tags_from_album.php
@@ -1,14 +1,14 @@
 <?php
 /** 
  * Provides a function to print a tag cloud of all image tags from an album optionally including the subalbums or the album tags including sub album tags.
- * Requires MySQL 5 or newer.
+ * Note: The optional counter prints the total number of the tag used, not just for the select album (as clicking on it will return all anyway.)
  *   
- * @author Malte Müller (acrylian) based on maxslug's FlickrishPrintAlbumTagCloud() from http://www.zenphoto.org/support/topic.php?id=6879#post-40363
+ * @author Malte Müller (acrylian)
  * @package plugins 
  */
 
-$plugin_description = gettext("Prints a tag cloud of all image tags from an album optionally including the subalbums or the album tags including sub album tags. Requires MySQL 5 or newer."); 
-$plugin_author = "Malte Müller (acrylian) based on maxslug's FlickrishPrintAlbumTagCloud()";
+$plugin_description = gettext("Prints a tag cloud of all image tags from an album optionally including the subalbums or the album tags including sub album tags. Note the optional counter prints the total number of the tag used, not just for the selected album (as clicking on it will return all anyway)."); 
+$plugin_author = "Malte Müller (acrylian)";
 $plugin_version = '1.3'; 
 $plugin_URL = "";
 
@@ -36,49 +36,49 @@ function getAllTagsFromAlbum($albumname="",$subalbums=false,$mode='images') {
 		$albumWhere = "WHERE `dynamic`=0 AND `show`=1".$passwordcheck;
 	}
 	if($subalbums) {
-		$albumWhere .= " AND `folder` LIKE '".$albumname."/%' ";
+		$albumWhere .= " AND `folder` LIKE '".$albumname."/%'";
 	} else {
 		$albumWhere .= " AND `folder` = '".$albumname."' ";
 	}
-	$albumids = query_full_array("SELECT id FROM " . prefix('albums'). $albumWhere);
+	$albumids = query_full_array("SELECT id, folder FROM " . prefix('albums'). $albumWhere);
 	$imageWhere = '';
 	switch($mode) {
 		case "images":
+			if(count($albumids) != 0) $imageWhere = " WHERE ";
+			$count = "";
 			foreach($albumids as $albumid) {
-				$imageWhere .= ' AND i.albumid='. $albumid['id'];
+				$count++;
+				$imageWhere .= 'albumid='. $albumid['id'];
+				if($count != count($albumids)) $imageWhere .= " OR ";
 			}
-			$sql = "SELECT t.tagid, tags.name, COUNT(*) AS count FROM ".
-           prefix('obj_to_tag').
-           " AS t LEFT JOIN (".
-           prefix('images'). "AS i, ".
-           prefix('tags').   "AS tags)".
-           " ON (t.objectid = i.id AND tags.id= t.tagid)".
-           " WHERE t.type = 'images' AND i.show = 1 ".
-           $imageWhere.
-           " GROUP BY t.tagid";
+			$imageids = query_full_array("SELECT id, albumid FROM " . prefix('images').$imageWhere);
+			$count = "";
+			$tagWhere = "";
+			if(count($imageids) != 0) $tagWhere = " WHERE ";
+			foreach($imageids as $imageid) {
+				$count++;
+				$tagWhere .= '(o.objectid ='. $imageid['id']." AND o.tagid = t.id AND o.type = 'images')";
+				if($count != count($imageids)) $tagWhere .= " OR ";
+			}
+			$tags = query_full_array("SELECT DISTINCT t.name, t.id, (SELECT DISTINCT COUNT(*) FROM ". prefix('obj_to_tag'). " WHERE tagid = t.id AND type = 'images') as count FROM  ". prefix('obj_to_tag'). "as o,". prefix('tags'). "as t".$tagWhere." ORDER by t.name");
 			break;
 		case "albums":
+			$count = "";
+			$tagWhere = "";
+			if(count($albumids) != 0) $tagWhere = " WHERE ";
 			foreach($albumids as $albumid) {
-				$imageWhere .= ' AND a.id='. $albumid['id'];
+				$count++;
+				$tagWhere .= '(o.objectid ='. $albumid['id']." AND o.tagid = t.id AND o.type = 'albums')";
+				if($count != count($albumids)) $tagWhere .= " OR ";
 			}
-			$sql = "SELECT t.tagid, tags.name, COUNT(*) AS count FROM ".
-           prefix('obj_to_tag').
-           " AS t LEFT JOIN (".
-           prefix('albums'). "AS a, ".
-           prefix('tags').   "AS tags)".
-           " ON (t.objectid = a.id AND tags.id= t.tagid)".
-           " WHERE t.type = 'albums'".
-           $imageWhere.
-           " GROUP BY t.tagid";
+			$tags = query_full_array("SELECT DISTINCT t.name, t.id, (SELECT DISTINCT COUNT(*) FROM ". prefix('obj_to_tag'). " WHERE tagid = t.id AND o.type = 'albums')  as count ". prefix('obj_to_tag'). "as o,". prefix('tags'). "as t".$tagWhere." ORDER by t.name");
 			break;
-		}
-    $tags = query_full_array($sql);
-    if (!is_array($tags)) {
-        return;
-    }
-    return $tags;
+	}
+	if (!is_array($tags)) {
+		return;
+	}
+	return $tags;
 }
-
 
 /** 
  * Prints a tag cloud list of the tags in one album and optionally its subalbums.
@@ -90,12 +90,13 @@ function getAllTagsFromAlbum($albumname="",$subalbums=false,$mode='images') {
  * @param string $separator how to separate the entries
  * @param string $class css classs to style the list 
  * @param integer $showcounter if the tag count should be shown (no counter if $mode = "all")
+ * @param bool $tagcloud if set to false a simple list without font size changes will be printed, set to true (default) prints a list as a tag cloud
  * @param integere $size_min smallest font size the cloud should display
  * @param integer $size_max largest font size the cloud should display
  * @param integer $count_min the minimum count for a tag to appear in the output 
  * @param integer $count_max the floor count for setting the cloud font size to $size_max
  */
-function printAllTagsFromAlbum($albumname="",$subalbums=false,$mode='images',$separator='',$class="",$showcounter=true,$size_min=0.5,$size_max=5,$count_min=1,$count_max=50) {
+function printAllTagsFromAlbum($albumname="",$subalbums=false,$mode='images',$separator='',$class="",$showcounter=true,$tagcloud=true,$size_min=1,$size_min=0.5,$size_max=5,$count_min=1,$count_max=50) {
 	if($mode == 'all') {
 		$showcounter = false;
 		$tags1 = getAllTagsFromAlbum($albumname,$subalbums,'albums');
@@ -110,30 +111,27 @@ function printAllTagsFromAlbum($albumname="",$subalbums=false,$mode='images',$se
 	$count_min = sanitize_numeric($count_min);
 	$count_max = sanitize_numeric($count_max);
 	$separator = sanitize($separator);
-	
 	$counter = '';
 	echo "<ul ".$class.">\n";
 	foreach ($tags as $row) {
 		$count = $row['count'];
-		$tid   = $row['tagid'];
+		$tid   = $row['id'];
 		$tname = $row['name'];
-		$size = min(max(round(($size_max*($count-$count_min))/($count_max-$count_min),
-		2), $size_min)
-		,$size_max);
-		$size = str_replace(',','.', $size);
+		$style = "";
+		if($tagcloud OR $mode == 'all') {
+			$size = min(max(round(($size_max*($count-$count_min))/($count_max-$count_min),
+			2), $size_min)
+			,$size_max);
+			$size = str_replace(',','.', $size);
+			$style = " style=\"font-size:".$size."em;\"";
+		}
 		if($showcounter) {
 			$counter = ' ('.$count.')';
 		}
-		if($mode == 'all') { // disable the dynamic font size for 'all' mode
-			$style = '';
-		} else {
-			$style = "style=\"font-size:".$size."em;\"";
-		}
-		echo "<li><a class=\"tagLink\" href=\"".htmlspecialchars(getSearchURL($tname, '', 'tags',''))."\" $style rel=\"nofollow\">".$tname.$counter."</a>".$separator."</li>\n";
+		echo "<li><a class=\"tagLink\" href=\"".htmlspecialchars(getSearchURL($tname, '', 'tags',''))."\"".$style." rel=\"nofollow\">".$tname.$counter."</a>".$separator."</li>\n";
 	}
 	echo "</ul>\n";
 }
-
 
 /** 
  * Removes duplicate entries in multi dimensional array. 
