11e479133493ce157a6d71b5820b2e409ebec56c
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2010-04-04 16:12:22 +0000

    Sitemap plugin updates: Internal restructuring, more options and also Zenpage news category pagination supported
    git-svn-id: http://www.zenphoto.org/svn/trunk@5202 46129fc4-274e-7f48-8cfa-5da4c607681c

--- sitemap.php
+++ sitemap.php
@@ -2,7 +2,6 @@
 require_once(dirname(__FILE__).'/zp-core/folder-definitions.php');
 define('OFFSET_PATH', 0);
 require_once(ZENFOLDER . "/template-functions.php");
-//require_once(ZENFOLDER . "/functions-rss.php");
 if(!getOption('zp_plugin_zenphoto-sitemap-extended')) {
 	header("HTTP/1.0 404 Not Found");
 	header("Status: 404 Not Found");
@@ -13,96 +12,19 @@ startSitemapCache(true);
 // Output content type and charset
 header('Content-Type: text/xml;charset=utf-8');
 // Output XML file headers, and plug the plugin :)
-echonl('<?xml version="1.0" encoding="UTF-8"?>');
-echonl('<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
+sitemap_echonl('<?xml version="1.0" encoding="UTF-8"?>');
+sitemap_echonl('<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
 
 // Add the front page of the gallery to the URL list
-echonl("\t<url>\n\t\t<loc>".FULLWEBPATH."</loc>\n\t\t<lastmod>".date('Y-m-d')."</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>");
-// getting index pages
-if(galleryAlbumsPerPage() != 0) {
-	$toplevelpages = ceil($_zp_gallery->getNumAlbums() / galleryAlbumsPerPage());
-} else {
-	$toplevelpages = false;
-}
-// print further index pages if avaiable
-if($toplevelpages) {
-	for($x = 2;$x <= $toplevelpages; $x++) {
-		$url = FULLWEBPATH.'/'.rewrite_path('page/'.$x,'index.php?page='.$x,false);
-		echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".date('Y-m-d')."</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>");
-	}
-}
-// password check required because subalbums inherit passwords from parents!
-$passwordcheck = '';
-$albumscheck = query_full_array("SELECT * FROM " . prefix('albums'). " ORDER BY title");
-foreach($albumscheck as $albumcheck) {
-	if(!checkAlbumPassword($albumcheck['folder'], $hint)) { //TODO If done as a single file this throws an fatal error because the internally used zp_loggedin() is still not defined
-		$albumpasswordcheck= " AND id != ".$albumcheck['id'];
-		$passwordcheck = $passwordcheck.$albumpasswordcheck;
-	}
-}
-$albumWhere = "WHERE `dynamic`=0 AND `show`=1".$passwordcheck;
-// Find public albums
-$albums = query_full_array('SELECT `folder`,`date` FROM ' . prefix('albums') . $albumWhere);
-foreach($albums as $album) {
-	$albumobj = new Album($_zp_gallery,$album['folder']);
-	//getting the album pages
-	set_context(ZP_ALBUM);
-  makeAlbumCurrent($albumobj);
-  $pageCount = getTotalPages();
-	$date = substr($albumobj->getDatetime(),0,10);
-	$url = FULLWEBPATH.'/'.rewrite_path(pathurlencode($albumobj->name),'?album='.pathurlencode($albumobj->name),false);
-	echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>");
-	// print album pages if avaiable
-	if($pageCount > 1) {
-		for($x = 2;$x <= $pageCount; $x++) {
-			$url = FULLWEBPATH.'/'.rewrite_path(pathurlencode($albumobj->name).'/page/'.$x,'?album='.pathurlencode($albumobj->name).'&amp;page='.$x,false);
-			echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>");
-		}
-	}
-	$images = $albumobj->getImages();
-	foreach($images as $image) {
-		$date = substr($album['date'],0,10);
-		$path = FULLWEBPATH.'/'.rewrite_path(pathurlencode($album['folder']).'/'.urlencode($image),'?album='.pathurlencode($album['folder']).'&amp;image='.urlencode($image),false);
-		echonl("\t<url>\n\t\t<loc>".$path."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>");
-	}
-}
-restore_context();
+sitemap_echonl("\t<url>\n\t\t<loc>".FULLWEBPATH."</loc>\n\t\t<lastmod>".date('Y-m-d')."</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>");
+printSitemapIndexLinks();
+printSitemapAlbumsAndImages();
 // Optional Zenpage stuff
 if(getOption('zp_plugin_zenpage')) {
-	//Zenpage pages
-	$pages = getPages(true);
-	foreach($pages as $page) {
-		$pageobj = new ZenpagePage($page['titlelink']);
-		$date = substr($pageobj->getDatetime(),0,10);
-		$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_PAGES.'/'.urlencode($page['titlelink']),'?p='.ZENPAGE_PAGES.'&amp;title='.urlencode($page['titlelink']),false);
-		echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>weekly</changefreq>\n\t</url>");
-	}
-	// zenpage news index
-	$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_NEWS,'?p='.ZENPAGE_NEWS,false);
-	echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".date('Y-m-d')."</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>");
-	// getting pages for the main news loop
-	$newspages = ceil(getTotalArticles() / getOption("zenpage_articles_per_page"));
-	if($newspages > 1) {
-		for($x = 2;$x <= $newspages; $x++) {
-			$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_NEWS.'/page/'.$x,'?p='.ZENPAGE_NEWS.'&amp;page='.$x,false);
-			echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".date('Y-m-d')."</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>");
-		}
-	}
-	//Zenpage news articles
-	$articles = getNewsArticles('','','published',true,"date","desc"); //query_full_array("SELECT titlelink, `date` FROM ".prefix('zenpage_news'));// normally getNewsArticles() should be user but has currently a bug in 1.2.9 regarding getting all articles...
-	foreach($articles as $article) {
-		$articleobj = new ZenpageNews($article['titlelink']);
-		$date = substr($articleobj->getDatetime(),0,10);
-		$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_NEWS.'/'.urlencode($articleobj->getTitlelink()),'?p='.ZENPAGE_NEWS.'&amp;title=' . urlencode($articleobj->getTitlelink()),false);
-		echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>daily</changefreq>\n\t</url>");
-	}
-	//Zenpage news categories
-	$newscats = getAllCategories();
-	// Add the correct URLs to the URL list
-	foreach($newscats as $newscat) {
-		$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_NEWS.'/category/'.urlencode($newscat['cat_link']),'?p='.ZENPAGE_NEWS.'&amp;category=' . urlencode($newscat['cat_link']),false);
-		echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<changefreq>weekly</changefreq>\n\t</url>");
-	}
+	printSitemapZenpagePages();
+	printSitemapZenpageNewsIndex();
+	printSitemapZenpageNewsArticles();
+	printSitemapZenpageNewsCategories();
 }
-echonl('</urlset>');// End off the <urlset> tag
+sitemap_echonl('</urlset>');// End off the <urlset> tag
 endSitemapCache(true);
--- zp-core/zp-extensions/zenphoto-sitemap-extended.php
+++ zp-core/zp-extensions/zenphoto-sitemap-extended.php
@@ -1,15 +1,25 @@
 <?php
 /**
  * Generates a sitemap.org compatible XML file, for use with Google and other search engines. It supports albums and images as well as optionally Zenpage pages, news articles and news categories.
+ * <?xml version="1.0" encoding="UTF-8"?>
+ *<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+ *  <url>
+ *    <loc>http://www.example.com/</loc>
+ *    <lastmod>2005-01-01</lastmod> // except for index, Zenpage news index and news categories as they don't have a date attached (optional anyway)
+ *    <changefreq>monthly</changefreq>
+ * </url>
+ *</urlset>
  *
  * Renders the sitemap if a gallery page is called with "<zenphoto>/sitemap.php" in the URL. The sitemap is cached as a xml file within the root "cache_html/sitemap" folder.
- *
+ * 
+ * NOTE: The index links may not match if using the options for "Zenpage news on index" or a "custom home page" that some themes provide! Also it does not "know" about "custom pages" outside Zenpage or any special custom theme setup!
+ * 
  * @author Malte Müller (acrylian) based on the plugin by Jeppe Toustrup (Tenzer) http://github.com/Tenzer/zenphoto-sitemap
  * @package plugins
  */
 
 $plugin_is_filter = 5;
-$plugin_description = 'Generates a sitemaps.org compatible XML file, for use with Google and other search engines. It supports albums and images as well as optionally Zenpage pages, news articles and news categories. Renders the sitemap if a gallery page is called with "<zenphoto>/sitemap.php" in the URL.';
+$plugin_description = 'Generates a sitemaps.org compatible XML file, for use with Google and other search engines. It supports albums and images as well as optionally Zenpage pages, news articles and news categories. Renders the sitemap if a gallery page is called with "<zenphoto>/sitemap.php" in the URL. NOTE: The index links may not match if using the Zenpage option "news on index" that some themes provide! Also it does not "know" about "custom pages" outside Zenpage or any special custom theme setup!!';
 $plugin_author = 'Malte Müller (acrylian) based on the <a href="http://github.com/Tenzer/zenphoto-sitemap">plugin</a> by Jeppe Toustrup (Tenzer)';
 $plugin_version = '1.3.0';
 $plugin_URL = 'http://www.zenphoto.org/documentation/plugins/_'.PLUGIN_FOLDER.'---zenphoto-sitemap-extended.php.html';
@@ -40,11 +50,84 @@ class sitemap {
 
 	function sitemap() {
 		setOptionDefault('sitemap_cache_expire', 86400);
+		setOptionDefault('sitemap_changefreq_index', 'daily');
+		setOptionDefault('sitemap_changefreq_albums', 'daily');
+		setOptionDefault('sitemap_changefreq_images', 'daily');
+		setOptionDefault('sitemap_changefreq_pages', 'weekly');
+		setOptionDefault('sitemap_changefreq_newsindex','daily');
+		setOptionDefault('sitemap_changefreq_news', 'daily');
+		setOptionDefault('sitemap_changefreq_newscats', 'weekly');
+		setOptionDefault('sitemap_lastmod_albums', 'mtime');
+		setOptionDefault('sitemap_lastmod_images', 'mtime');
 	}
 
 	function getOptionsSupported() {
 		return array(	gettext('Sitemap cache expire') => array('key' => 'sitemap_cache_expire', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext("When the cache should expire in seconds. Default is 86400 seconds (1 day  = 24 hrs * 60 min * 60 sec).The cache can also be cleared on the admin overview page manually."))
+										'desc' => gettext("When the cache should expire in seconds. Default is 86400 seconds (1 day  = 24 hrs * 60 min * 60 sec).The cache can also be cleared on the admin overview page manually.")),
+		gettext('Change frequence - Zenphoto index') => array('key' => 'sitemap_changefreq_index', 'type' => OPTION_TYPE_SELECTOR,
+										'selections' => array(gettext("always")=>"always", 
+																					gettext("hourly")=>"hourly",
+																					gettext("daily")=>"daily", 
+																					gettext("weekly")=>"monthly",
+																					gettext("yearly")=>"yearly", 
+																					gettext("never")=>"never"),
+										'desc' => ''),
+		gettext('Change frequence - albums') => array('key' => 'sitemap_changefreq_albums', 'type' => OPTION_TYPE_SELECTOR,
+										'selections' => array(gettext("always")=>"always", 
+																					gettext("hourly")=>"hourly",
+																					gettext("daily")=>"daily", 
+																					gettext("weekly")=>"monthly",
+																					gettext("yearly")=>"yearly", 
+																					gettext("never")=>"never"),
+										'desc' => ''),		
+		gettext('Change frequence - images') => array('key' => 'sitemap_changefreq_images', 'type' => OPTION_TYPE_SELECTOR,
+										'selections' => array(gettext("always")=>"always", 
+																					gettext("hourly")=>"hourly",
+																					gettext("daily")=>"daily", 
+																					gettext("weekly")=>"monthly",
+																					gettext("yearly")=>"yearly", 
+																					gettext("never")=>"never"),
+										'desc' => ''),										
+		gettext('Change frequence - Zenpage pages') => array('key' => 'sitemap_changefreq_pages', 'type' => OPTION_TYPE_SELECTOR,
+										'selections' => array(gettext("always")=>"always", 
+																					gettext("hourly")=>"hourly",
+																					gettext("daily")=>"daily", 
+																					gettext("weekly")=>"monthly",
+																					gettext("yearly")=>"yearly", 
+																					gettext("never")=>"never"),
+										'desc' => ''),	
+		gettext('Change frequence - Zenpage news index') => array('key' => 'sitemap_changefreq_newsindex', 'type' => OPTION_TYPE_SELECTOR,
+										'selections' => array(gettext("always")=>"always", 
+																					gettext("hourly")=>"hourly",
+																					gettext("daily")=>"daily", 
+																					gettext("weekly")=>"monthly",
+																					gettext("yearly")=>"yearly", 
+																					gettext("never")=>"never"),
+										'desc' => ''),		
+		gettext('Change frequence: Zenpage news articles') => array('key' => 'sitemap_changefreq_news', 'type' => OPTION_TYPE_SELECTOR,
+										'selections' => array(gettext("always")=>"always", 
+																					gettext("hourly")=>"hourly",
+																					gettext("daily")=>"daily", 
+																					gettext("weekly")=>"monthly",
+																					gettext("yearly")=>"yearly", 
+																					gettext("never")=>"never"),
+										'desc' => ''),	
+		gettext('Change frequence - Zenpage news categories') => array('key' => 'sitemap_changefreq_newscats', 'type' => OPTION_TYPE_SELECTOR,
+										'selections' => array(gettext("always")=>"always", 
+																					gettext("hourly")=>"hourly",
+																					gettext("daily")=>"daily", 
+																					gettext("weekly")=>"monthly",
+																					gettext("yearly")=>"yearly", 
+																					gettext("never")=>"never"),
+										'desc' => ''),	
+	gettext('Last modification date - albums') => array('key' => 'sitemap_lastmod_albums', 'type' => OPTION_TYPE_SELECTOR,
+										'selections' => array(gettext("date")=>"date", 
+																					gettext("mtime")=>"mtime"),
+										'desc' => ''),			
+	gettext('Last modification date - images') => array('key' => 'sitemap_lastmod_images', 'type' => OPTION_TYPE_SELECTOR,
+										'selections' => array(gettext("date")=>"date", 
+																					gettext("mtime")=>"mtime"),
+										'desc' => ''),																																							
 		);
 	}
 
@@ -58,6 +141,7 @@ class sitemap {
  * @return array
  */
 function sitemap_cache_purgebutton($buttons) {
+	if(zp_loggedin(ADMIN_RIGHTS)) { 
 	$buttons[] = array(
 								'button_text'=>gettext('Purge sitemap cache'),
 								'formname'=>'clearcache_button',
@@ -69,25 +153,254 @@ function sitemap_cache_purgebutton($buttons) {
 								'rights'=> ADMIN_RIGHTS
 	);
 	return $buttons;
+	}
 }
 
 /**
  * Simple helper function which simply outputs a string and ends it of with a new-line.
- * @param  $string String
- * @return void
+ * @param  string $string text string
+ * @return string
  */
-function echonl($string) {
+function sitemap_echonl($string) {
 	echo($string . "\n");
+}
+/**
+ * Checks the changefreq value if entered manually and makes sure it is only one of the supported regarding sitemap.org
+ * @param  string $changefreq One of the supported changefrequence values regarding sitemap.org. Default is empty or wrong is "daily".
+ * @return string
+ */
+function sitemap_getChangefreq($changefreq='') {
+	$changefreq = sanitize($changefreq);
+	switch($changefreq) {
+		case 'always':
+		case 'hourly':
+		case 'daily':
+		case 'weekly':
+		case 'monthly':
+		case 'yearly':
+		case 'never':
+			$changefreq = $changefreq;
+			break;
+		default:
+			$changefreq = 'daily';
+			break;
+	}
+	return $changefreq;
+}
+/**
+ * Gets the dateformat for images and albums only.
+ * @param object $obj image or album object
+ * @param  string $option "date" or "mtime". If "mtime" is discovered to be not set, the date values is taken instead so we don't get 1970-01-10 dates
+ * @return string
+ */
+function sitemap_getDateformat($obj,$option) {
+	$date = '';
+	switch($option) {
+		case 'date':
+		default:
+			$date = substr($obj->getDatetime(),0,10);
+			break;
+		case 'mtime':
+			$timestamp = $obj->get('mtime');
+			if($timestamp == 0) {
+				$date = substr($obj->getDatetime(),0,10);
+			} else {
+				$date = strftime('%Y-%m-%d',$timestamp);
+			}
+			break;
+	}
+	return $date;
+}
+/**
+ * Prints the links to the index of a Zenphoto gallery incl. pagination
+ *@param  string $changefreq One of the supported changefrequence values regarding sitemap.org. Default is empty or wrong is "daily".
+ * @return string
+ */
+function printSitemapIndexLinks($changefreq='') {
+	global $_zp_gallery;
+	if(empty($changefreq)) {
+		$changefreq = getOption('sitemap_changefreq_index');
+	} else {
+		$changefreq = sitemap_getChangefreq($changefreq);
+	}
+	if(galleryAlbumsPerPage() != 0) {
+		$toplevelpages = ceil($_zp_gallery->getNumAlbums() / galleryAlbumsPerPage());
+	} else {
+		$toplevelpages = false;
+	}
+	// print further index pages if avaiable
+	if($toplevelpages) {
+		for($x = 2;$x <= $toplevelpages; $x++) {
+			$url = FULLWEBPATH.'/'.rewrite_path('page/'.$x,'index.php?page='.$x,false);
+			sitemap_echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".date('Y-m-d')."</lastmod>\n\t\t<changefreq>".$changefreq."</changefreq>\n\t</url>");
+		}
+	}
 }
 
 /**
- * A simple wrapper function for urlencode, which allows us to use it with array_walk().
- * @param  $value The array value from array_walk()
- * @param  $key The array key from array_walk() - Not used
- * @return void
+ * Prints links to all albums incl. pagination and their images
+ * @param  string $albumchangefreq One of the supported changefrequence values regarding sitemap.org. Default is empty or wrong is "daily".
+ * @param  string $imagechangefreq One of the supported changefrequence values regarding sitemap.org. Default is empty or wrong is "daily".
+ * @param  string $albumlastmod "date or "mtime"
+ * @param  string $imagelastmod "date or "mtime"
+ * @return string
  */
-function urlencodeWrapper(&$value, $key) {
-	$value = urlencode($value);
+function printSitemapAlbumsAndImages($albumchangefreq='',$imagechangefreq='',$albumlastmod='',$imagelastmod='') {
+	global $_zp_gallery, $_zp_current_album;
+	if(empty($albumchangefreq)) {
+		$albumchangefreq = getOption('sitemap_changefreq_albums');
+	} else {
+		$albumchangefreq = sitemap_getChangefreq($albumchangefreq);
+	}
+	if(empty($imagechangefreq)) {
+		$imagechangefreq = getOption('sitemap_changefreq_images');
+	} else {
+		$imagechangefreq = sitemap_getChangefreq($imagechangefreq);
+	}
+	if(empty($albumlastmod)) {
+		$albumlastmod = getOption('sitemap_lastmod_albums');
+	} else {
+		$albumlastmod = sanitize($albumlastmod);
+	}	
+	if(empty($imagelastmod)) {
+		$imagelastmod = getOption('sitemap_lastmod_images');
+	} else {
+		$imagelastmod = sanitize($imagelastmod);
+	}	
+	$passwordcheck = '';
+	$albumscheck = query_full_array("SELECT * FROM " . prefix('albums'). " ORDER BY title");
+	foreach($albumscheck as $albumcheck) {
+		if(!checkAlbumPassword($albumcheck['folder'], $hint)) { 
+		$albumpasswordcheck= " AND id != ".$albumcheck['id'];
+		$passwordcheck = $passwordcheck.$albumpasswordcheck;
+		}
+	}
+	$albumWhere = "WHERE `dynamic`=0 AND `show`=1".$passwordcheck;
+	// Find public albums
+	$albums = query_full_array('SELECT `folder`,`date` FROM ' . prefix('albums') . $albumWhere);
+	if($albums) {
+		foreach($albums as $album) {
+			$albumobj = new Album($_zp_gallery,$album['folder']);
+			//getting the album pages
+			set_context(ZP_ALBUM);
+			makeAlbumCurrent($albumobj);
+			$pageCount = getTotalPages();
+			$date = sitemap_getDateformat($albumobj,$albumlastmod);
+			$url = FULLWEBPATH.'/'.rewrite_path(pathurlencode($albumobj->name),'?album='.pathurlencode($albumobj->name),false);
+			sitemap_echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>".$albumchangefreq."</changefreq>\n\t</url>");
+			// print album pages if avaiable
+			if($pageCount > 1) {
+				for($x = 2;$x <= $pageCount; $x++) {
+					$url = FULLWEBPATH.'/'.rewrite_path(pathurlencode($albumobj->name).'/page/'.$x,'?album='.pathurlencode($albumobj->name).'&amp;page='.$x,false);
+					sitemap_echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>".$albumchangefreq."</changefreq>\n\t</url>");
+				}
+			}
+			$images = $albumobj->getImages();
+			if($images) {
+				foreach($images as $image) {
+					$imageob = newImage($albumobj,$image);
+					$date = sitemap_getDateformat($imageob,$imagelastmod);
+					$path = FULLWEBPATH.'/'.rewrite_path(pathurlencode($albumobj->name).'/'.urlencode($imageob->filename),'?album='.pathurlencode($albumobj->name).'&amp;image='.urlencode($imageob->filename),false);
+					sitemap_echonl("\t<url>\n\t\t<loc>".$path."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>".$imagechangefreq."</changefreq>\n\t</url>");
+				}
+			}
+		}
+	}
+	restore_context();
+}
+/**
+ * Prints links to all Zenpage pages
+ * @param  string $changefreq One of the supported changefrequence values regarding sitemap.org. Default is empty or wrong is "daily".
+ * @return string
+ */
+function printSitemapZenpagePages($changefreq='') {
+	if(empty($changefreq)) {
+		$changefreq = getOption('sitemap_changefreq_pages');
+	} else {
+		$changefreq = sitemap_getChangefreq($changefreq);
+	}
+	$pages = getPages(true);
+	if($pages) {
+		foreach($pages as $page) {
+			$pageobj = new ZenpagePage($page['titlelink']);
+			$date = substr($pageobj->getDatetime(),0,10);
+			$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_PAGES.'/'.urlencode($page['titlelink']),'?p='.ZENPAGE_PAGES.'&amp;title='.urlencode($page['titlelink']),false);
+			sitemap_echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>".$changefreq."</changefreq>\n\t</url>");
+		}
+	}
+}
+/**
+ * Prints links to the main Zenpage news index incl. pagination
+ * @param  string $changefreq One of the supported changefrequence values regarding sitemap.org. Default is empty or wrong is "daily".
+ * @return string
+ */
+function printSitemapZenpageNewsIndex($changefreq='') {
+	if(empty($changefreq)) {
+		$changefreq = getOption('sitemap_changefreq_newsindex');
+	} else {
+		$changefreq = sitemap_getChangefreq($changefreq);
+	}
+	$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_NEWS.'/1','?p='.ZENPAGE_NEWS.'&amp;page=1',false);
+	sitemap_echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".date('Y-m-d')."</lastmod>\n\t\t<changefreq>".$changefreq."</changefreq>\n\t</url>");
+	// getting pages for the main news loop
+	$newspages = ceil(getTotalArticles() / getOption("zenpage_articles_per_page"));
+	if($newspages > 1) {
+		for($x = 2;$x <= $newspages; $x++) {
+			$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_NEWS.'/'.$x,'?p='.ZENPAGE_NEWS.'&amp;page='.$x,false);
+			sitemap_echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".date('Y-m-d')."</lastmod>\n\t\t<changefreq>".$changefreq."</changefreq>\n\t</url>");
+		}
+	}
+}
+/**
+ * Prints to the Zenpage news articles
+ * @param  string $changefreq One of the supported changefrequence values regarding sitemap.org. Default is empty or wrong is "daily".
+ * @return string
+ */
+function printSitemapZenpageNewsArticles($changefreq='') {
+	if(empty($changefreq)) {
+		$changefreq = getOption('sitemap_changefreq_news');
+	} else {
+		$changefreq = sitemap_getChangefreq($changefreq);
+	}
+	$articles = getNewsArticles('','','published',true,"date","desc"); //query_full_array("SELECT titlelink, `date` FROM ".prefix('zenpage_news'));// normally getNewsArticles() should be user but has currently a bug in 1.2.9 regarding getting all articles...
+	if($articles) {
+		foreach($articles as $article) {
+			$articleobj = new ZenpageNews($article['titlelink']);
+			$date = substr($articleobj->getDatetime(),0,10);
+			$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_NEWS.'/'.urlencode($articleobj->getTitlelink()),'?p='.ZENPAGE_NEWS.'&amp;title=' . urlencode($articleobj->getTitlelink()),false);
+			sitemap_echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<lastmod>".$date."</lastmod>\n\t\t<changefreq>".$changefreq."</changefreq>\n\t</url>");
+		}
+	}
+}
+
+/**
+ * Prints links to Zenpage news categories incl. pagination
+ * @param  string $changefreq One of the supported changefrequence values regarding sitemap.org. Default is empty or wrong is "daily".
+ * @return string
+ */
+function printSitemapZenpageNewsCategories($changefreq='') {
+	if(empty($changefreq)) {
+		$changefreq = getOption('sitemap_changefreq_newscats');
+	} else {
+		$changefreq = sitemap_getChangefreq($changefreq);
+	}
+	$newscats = getAllCategories();
+	if($newscats) {
+		// Add the correct URLs to the URL list
+		foreach($newscats as $newscat) {
+			$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_NEWS.'/category/'.urlencode($newscat['cat_link']).'/1','?p='.ZENPAGE_NEWS.'&amp;category=' . urlencode($newscat['cat_link']).'&amp;page=1',false);
+			sitemap_echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<changefreq>".$changefreq."</changefreq>\n\t</url>");
+			// getting pages for the categories
+			$articlecount = countArticles($newscat['cat_link']);
+			$catpages = ceil($articlecount / getOption("zenpage_articles_per_page"));
+			if($catpages > 1) {
+				for($x = 2;$x <= $catpages ; $x++) {
+					$url = FULLWEBPATH.'/'.rewrite_path(ZENPAGE_NEWS.'/category/'.urlencode($newscat['cat_link']).'/'.$x,'?p='.ZENPAGE_NEWS.'&amp;category=' . urlencode($newscat['cat_link']).'&amp;page='.$x,false);
+					sitemap_echonl("\t<url>\n\t\t<loc>".$url."</loc>\n\t\t<changefreq>".$changefreq."</changefreq>\n\t</url>");
+				}
+			}
+		}
+	}
 }
 
 /**
