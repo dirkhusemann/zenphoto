c54709ca2b7278888e6257d638abe3b059c3f98c
acrylian <acrylian@46129fc4-274e-7f48-8cfa-5da4c607681c>
2010-05-07 17:01:00 +0000

    minor corrections for utilities
    git-svn-id: http://www.zenphoto.org/svn/trunk@5389 46129fc4-274e-7f48-8cfa-5da4c607681c

--- zp-core/utilities/purge_rss_cache.php
+++ zp-core/utilities/purge_rss_cache.php
@@ -2,7 +2,7 @@
 $button_text = gettext("Purge RSS cache");
 $button_action = WEBPATH.'/'.ZENFOLDER.'/admin.php?action=clear_rss_cache';
 $button_icon = 'images/edit-delete.png'; 
-$button_title = gettext("Sets all hitcounters to zero.");
+$button_title = gettext("Purge RSS cache");
 $button_alt = gettext("Purge RSS cache");
 $button_hidden = '<input type="hidden" name="action" value="clear_rss_cache" />';
 $button_rights = ADMIN_RIGHTS;
--- zp-core/utilities/refresh_metadata.php
+++ zp-core/utilities/refresh_metadata.php
@@ -2,7 +2,7 @@
 $button_text = gettext("Refresh Metadata");
 $button_action = WEBPATH.'/'.ZENFOLDER.'/admin-refresh-metadata.php';
 $button_icon = 'images/refresh.png'; 
-$button_title = gettext("Sets all hitcounters to zero.");
+$button_title = gettext("Forces a refresh of the metadata for all images and albums.");
 $button_alt = gettext("Forces a refresh of the metadata for all images and albums.");
 $button_rights = ADMIN_RIGHTS;
 ?>
